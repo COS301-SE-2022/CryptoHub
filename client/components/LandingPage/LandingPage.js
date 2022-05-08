97c9a7fe4930ece8d01e5690e30ba879801be7b0
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2022-05-08 19:30:21 +0200

    commented out carousel

--- client/components/LandingPage/LandingPage.js
+++ client/components/LandingPage/LandingPage.js
@@ -5,16 +5,15 @@ const LandingPage = () => {
   return (
     <div className="flex flex-col h-40 p-24 items-center h-8/12 justify-between">
       <div className="flex flex-col items-center">
-        <h1 className="text-5xl">
-          Welcome to{" "}
-          <span className="text-indigo-600 font-semibold">CrytoHub</span>
+        <h1 className="text-5xl font-bold">
+          Welcome to <span className="text-indigo-600">CrytoHub</span>
         </h1>
         <p className="text-md text-gray-600 mt-4">
           Explore the world of cryptocurrencies
         </p>
       </div>
-      <div className="bg-indigo-600 w-8/12 rounded-md justify-center flex mt-16">
-        <Carousel />
+      <div className="w-8/12 rounded-md justify-center flex mt-16 p-6">
+        {/* <Carousel /> */}
       </div>
     </div>
   );
