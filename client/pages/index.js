9bd01cc834cb11d33f4ef9f18b94ef77947c7a75
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2022-05-08 19:52:31 +0200

    added login screen

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,8 @@
-import Head from "next/head";
-import Image from "next/image";
+import { useState } from "react";
+import Layout from "../components/Layout";
+import LandingPage from "../components/LandingPage/LandingPage";
 
-export default function Home() {
-  return <div></div>;
+export default function Home({ Component, pageProps }) {
+  const [isUserLoggedIn] = useState(false);
+  return <>{isUserLoggedIn ? <Layout></Layout> : <LandingPage />}</>;
 }
---
+++ client/pages/signup.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Signup = () => {
+  return <div>signup</div>;
+};
+
+export default Signup;
