00613688d0c3eb93e532d5fabf95c7f73d747582
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2022-05-08 21:06:47 +0200

    Update NavigationProfile.js

--- client/components/NavigationBar/NavigationProfile.js
+++ client/components/NavigationBar/NavigationProfile.js
@@ -35,7 +35,7 @@ const NavigationProfile = () => {
                 href="#"
                 className={classNames(
                   active ? "bg-gray-100" : "",
-                  "block px-4 py-2 text-sm text-gray-700"
+                  "block px-4 py-2 text-sm text-gray-700 w-full"
                 )}
               >
                 Sign out
