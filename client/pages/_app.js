26486a8a3b9e0f9174d5957ded13298fcff54494
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2022-05-08 19:20:44 +0200

    Added carousel to landing page

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,6 +2,7 @@ import Layout from "../components/Layout";
 import LandingPage from "../components/LandingPage/LandingPage";
 import "../styles/globals.css";
 import { useState } from "react";
+import "../components/LandingPage/Carousel.css";
 
 function MyApp({ Component, pageProps }) {
   const [isUserLoggedIn] = useState(false);
