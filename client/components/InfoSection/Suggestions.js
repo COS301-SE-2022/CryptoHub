360004a08ed24a5646465a2ef8b868c245e8fced
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2022-05-10 21:56:03 +0200

    added suggested accounts section

--- client/components/InfoSection/Suggestions.js
+++ client/components/InfoSection/Suggestions.js
@@ -1,9 +1,16 @@
 import React from "react";
+import SuggestedAccount from "./SuggestedAccount";
+import { mockSuggestedAccounts } from "../../mocks/mockSuggestedAccounts";
 
 const Suggestions = () => {
   return (
     <div>
-      <p className="text-md font-bold text-indigo-600">Suggestions</p>
+      <p className="text-md font-bold text-indigo-600 mb-2 overflow-auto">
+        Suggestions
+      </p>
+      {mockSuggestedAccounts.map((data, index) => {
+        return <SuggestedAccount key={index} name={data.name} />;
+      })}
     </div>
   );
 };
