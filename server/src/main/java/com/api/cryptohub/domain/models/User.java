144959ae8fefad2364ab2dbf2cc6ce62fe37fd77
Thabo  Jerome Chesane <u20507102@tuks.co.za>
2022-05-10 23:17:40 +0200

    Sequences
    Sequences Added

--- server/src/main/java/com/api/cryptohub/domain/models/User.java
+++ server/src/main/java/com/api/cryptohub/domain/models/User.java
@@ -1,14 +1,26 @@
 package com.api.cryptohub.domain.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.SEQUENCE;
 
 @Entity(name = "User")
 @Table(name="users")
 public class User {
 
     @Id
+    @SequenceGenerator(
+            name =  "users_sequence",
+            sequenceName  = "users_sequence",
+            allocationSize = 1
+
+    )
+
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "users_sequence"
+
+    )
     private Integer userId;
     private String fistName;
     private String lastName;
