14838d58279c5e07c0d040493dc0b46971e9589e
Casparus <u20780479@tuks.co.za>
2022-05-10 15:37:14 +0200

    (Unit Test) Added AuthorizationController.Response Unit test

--- server/src/test/java/com/api/cryptohub/CryptoHubApplicationTests.java
+++ server/src/test/java/com/api/cryptohub/CryptoHubApplicationTests.java
@@ -1,13 +1,20 @@
 package com.api.cryptohub;
 
+import com.api.cryptohub.api.controllers.AuthorizationController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CryptoHubApplicationTests {
 
     @Test
     void contextLoads() {
+        //AuthorizationController controller = new AuthorizationController();
+        AuthorizationController.Response controller = new AuthorizationController.Response("Test");
+        String response = controller.getResponse();
+        assertEquals("Test", response);
     }
 
 }
