ef21ac88d185edd35fc7c55f61eb472789f240bc
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2022-05-10 12:44:00 +0200

    Update CreatePostButton.js

--- client/components/NavigationBar/CreatePostButton.js
+++ client/components/NavigationBar/CreatePostButton.js
@@ -16,7 +16,7 @@ const CreatePostButton = () => {
       {showModal ? (
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-6/12 my-6 mx-auto max-w-3xl">
+            <div className="relative sm:w-6/12 my-6 mx-auto max-w-3xl">
               <div className="border-0 rounded-lg shadow-sm relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 <div className="flex items-start justify-between p-5 border-solid border-slate-200 rounded-t">
                   <h2>Post about the latest crypto news</h2>
