7046b6569063aaca585b71584e83c766ebc7ecb6
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2022-05-10 12:17:24 +0200

    (client): added breakpoints to landing page

--- client/components/LandingPage/LandingPage.js
+++ client/components/LandingPage/LandingPage.js
@@ -3,16 +3,16 @@ import Carousel from "./Carousel";
 
 const LandingPage = () => {
   return (
-    <div className="flex flex-col h-40 p-24 items-center h-8/12 justify-between">
+    <div className="flex flex-col h-40 px-10 py-32 sm:p-24 items-center h-8/12 justify-between">
       <div className="flex flex-col items-center">
-        <h1 className="text-5xl font-bold">
-          Welcome to <span className="text-indigo-600">CrytoHub</span>
+        <h1 className="sm:text-5xl text-2xl font-bold text-center">
+          Welcome to <span className="text-indigo-600 ">CrytoHub</span>
         </h1>
         <p className="text-md text-gray-600 mt-4">
           Explore the world of cryptocurrencies
         </p>
       </div>
-      <div className="w-8/12 rounded-md justify-center flex mt-8">
+      <div className="w-8/12 rounded-md justify-center items-center sm:flex mt-8">
         <div className="inline-flex rounded-md shadow">
           <a
             href="/signup"
