cb898208c76a199ba184af58a3e7d89a78621a6c
Thabo  Jerome Chesane <u20507102@tuks.co.za>
2022-05-12 00:03:34 +0200

    used rand function to mock like and comment number

--- client/components/Posts/Post.js
+++ client/components/Posts/Post.js
@@ -11,7 +11,17 @@ const Post = ({ name, content, likes, comments, liked, image }) => {
         <p className="text-sm font-semibold mb-2 translate-y-1 ml-2">{name}</p>
       </div>
       <p className="text-sm">{content}</p>
-
+      <div className="flex flex-row mt-4">
+        <button className="text-sm mr-4 flex flex-row">
+          <HeartIcon className="h-5 w-5 text-black " />{" "}
+          {""}
+          <p className="ml-1">{Math.floor(Math.random() * 300)} likes</p>
+        </button>
+        <button className="text-sm flex flex-row">
+          <ChatIcon className="h-5 w-5 text-black " /> {""}
+          <p className="ml-1">{Math.floor(Math.random() * 50)} comments</p>
+        </button>
+      </div>
     </div>
   );
 };
