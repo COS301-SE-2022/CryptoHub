98f347bce1e6d7c19d464ee82a616bbb8a2f1685
DanieldeNobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-05-13 01:12:44 +0200

    Updated post for mock data

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -7,13 +7,15 @@ VALUES ('John', 'Doe', 'JohnDoe@gmail.com', '123', 'John Doe'),
        ('Midge', 'Abigail', 'MidgeAbigail@gmail.com', 'Password', 'Midge Abigail');
 
 INSERT INTO posts (user_id, post)
-VALUES (1, 'I like this coin'),
-       (1, 'Doggy Coin is the future'),
-       (1, 'Bitcoin is plummeting'),
-       (1, 'The is a reflection of the world state'),
-       (2, 'we are in need of cryptos more than ever'),
-       (2, 'How do i make money'),
-       (3, 'Awesome site');
+VALUES (1, 'Bitcoin Drops to 2020 Levels of Near $24K'),
+       (1, 'More than $200bn wiped off cryptocurrency market in a day'),
+       (1, 'Bitcoin investors are panicking as a controversial crypto experiment unravels'),
+       (2, 'SEC boosts crypto unit, Ethereum gas fees soar, and Congress’ stance on crypto'),
+       (3, 'Can crypto go green? Major companies are trying — but it’s easier said than done'),
+       (4, 'Fort Worth pilots bitcoin mine and dogecoin whipsaws after Elon Musk buys Twitter'),
+       (4, 'U.S. Treasury sanctions crypto miner BitRiver and Binance limits Russian accounts'),
+       (5, 'Miami wants to become a crypto hub and Coinbase launches an NFT marketplace'),
+       (6, 'The pros and cons of getting paid in crypto, plus Elon Musk’s Twitter takeover');
 
 -- INSERT INTO users_following (user_userid, following_userid)
 -- VALUES (1, 2),
