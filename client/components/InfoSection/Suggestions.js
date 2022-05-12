de35ff2e9e0764599c61da3748fed888b7a51f40
DanieldeNobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-05-12 22:10:18 +0200

    Added API to get all users.

--- client/components/InfoSection/Suggestions.js
+++ client/components/InfoSection/Suggestions.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import SuggestedAccount from "./SuggestedAccount";
 import { mockSuggestedAccounts } from "../../mocks/mockSuggestedAccounts";
 
 const Suggestions = () => {
+  const [accounts, setAccounts] = useState([]);
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+    };
+
+    fetch("http://localhost:8082/api/user/getallusers", options)
+      .then((response) => response.json())
+      .then((data) => {
+        console.warn(data);
+        setAccounts(data);
+      })
+      .catch((error) => {
+        console.warn("Error", error);
+      });
+  }, []);
   return (
     <div>
       <p className="text-md font-bold text-indigo-600 mb-2 overflow-auto">
