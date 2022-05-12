120ec3cda6b9da02e422fb24577fd5c6b1b2c5c2
DanieldeNobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-05-12 22:18:16 +0200

    Added api call to get followers

--- client/components/InfoSection/Suggestions.js
+++ client/components/InfoSection/Suggestions.js
@@ -1,14 +1,27 @@
 import React, { useState, useEffect, useContext } from "react";
 import SuggestedAccount from "./SuggestedAccount";
 import { mockSuggestedAccounts } from "../../mocks/mockSuggestedAccounts";
+import { userContext } from "../../auth/auth";
 
 const Suggestions = () => {
+  const { user } = useContext(userContext);
   const [accounts, setAccounts] = useState([]);
+  const [followers, setFollowers] = useState([]);
 
   useEffect(() => {
     const options = {
       method: "GET",
     };
+
+    fetch(`http://localhost:8082/api/user/getfollowers/${user.id}`, options)
+      .then((response) => response.json())
+      .then((data) => {
+        console.warn("followers", data);
+        setFollowers(data);
+      })
+      .catch((error) => {
+        console.warn("Error", error);
+      });
 
     fetch("http://localhost:8082/api/user/getallusers", options)
       .then((response) => response.json())
