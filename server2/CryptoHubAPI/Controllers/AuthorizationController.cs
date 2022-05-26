5c9286c93f008cc886c1e713d26e95d2a831fb2d
Casparus <u20780479@tuks.co.za>
2022-05-26 21:57:23 +0200

    Added Login method for AuthorizationController

--- server2/CryptoHubAPI/Controllers/AuthorizationController.cs
+++ server2/CryptoHubAPI/Controllers/AuthorizationController.cs
@@ -1,6 +1,47 @@
-﻿namespace CryptoHubAPI.Controllers
+﻿using Domain.IRepository;
+using Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoHubAPI.Controllers
 {
-    public class Class
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class AuthorizationController : Controller
     {
+
+        private readonly IUserRepository userRepository;
+
+        public AuthorizationController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Response<User>> Login([FromBody] User user)
+        {
+            var response = await userRepository.FindOne(u => u.Email == user.Email);
+
+            if (response == null)
+                return BadRequest(new Response<User>
+                {
+                    HasError = true,
+                    Message = "incorrect username or password",
+                    Model = null
+                });
+
+        }
     }
+    public class Response <T>
+    {
+        public string Message { get; set; }
+        public Boolean HasError { get; set; }
+        public T Model { get; set; }
+
+        public Response()
+        {
+
+        }
+    }
+
 }
