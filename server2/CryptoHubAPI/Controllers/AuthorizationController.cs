683fe486ffa75fcd242429006389f62f25e442e5
Casparus <u20780479@tuks.co.za>
2022-05-26 22:03:26 +0200

    Added Register method for AuthorizationController

--- server2/CryptoHubAPI/Controllers/AuthorizationController.cs
+++ server2/CryptoHubAPI/Controllers/AuthorizationController.cs
@@ -18,18 +18,54 @@ namespace CryptoHubAPI.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<Response<User>> Login([FromBody] User user)
+        public async Task<ActionResult<Response<User>>> Login([FromBody] User user)
         {
-            var response = await userRepository.FindOne(u => u.Email == user.Email);
+            var loginUser = await userRepository.FindOne(u => u.Email == user.Email);
 
-            if (response == null)
+            if (loginUser == null)
                 return BadRequest(new Response<User>
                 {
                     HasError = true,
                     Message = "incorrect username or password",
                     Model = null
                 });
+            if(!(loginUser.Password == user.Password))
+            {
+                return BadRequest(new Response<User>
+                {
+                    HasError = true,
+                    Message = "incorrect username or password",
+                    Model = null
+                });
+            }
+            return Ok(new Response<User>
+            {
+                HasError = false,
+                Message = "logged in",
+                Model = loginUser
+            });
+        }
 
+        [HttpPost]
+        public async Task<ActionResult<Response<User>>> Register([FromBody] User user)
+        {
+            var registerUser = await userRepository.FindOne(u => u.Email == user.Email);
+
+            if (registerUser != null)
+                return BadRequest(new Response<User>
+                {
+                    HasError = true,
+                    Message = "user already exists",
+                    Model = null
+                });
+            await userRepository.Add(user);
+
+            return Ok(new Response<User>
+            {
+                HasError = false,
+                Message = "registered",
+                Model = registerUser
+            });
         }
     }
     public class Response <T>
