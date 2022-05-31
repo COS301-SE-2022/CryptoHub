9684b87d95e5988a0aac4206b42593ee9cbb30c3
Daniel de Nobrega <u20529440@gmail.com>
2022-05-31 20:55:29 +0200

    Created a component for coin.js

---
+++ client/components/CoinAccount/CoinInfo.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { HeartIcon, ChatIcon } from "@heroicons/react/outline";
+
+const Post = ({ name, price }) => {
+  return <div className="bg-white m-4 p-4 rounded-lg"></div>;
+};
+
+export default CoinInfo;
