af560d5d4e1aa4ed97a62edb4e0541360edd37e1
Daniel de Nobrega <u20529440@gmail.com>
2022-05-31 20:56:24 +0200

    Updated CoinInfo.js

--- client/components/CoinAccount/CoinInfo.js
+++ client/components/CoinAccount/CoinInfo.js
@@ -3,6 +3,10 @@ import { HeartIcon, ChatIcon } from "@heroicons/react/outline";
 
 const Post = ({ name, price }) => {
   return <div className="bg-white m-4 p-4 rounded-lg"></div>;
+  <div className="flex flew-row items-center mb-2">
+    <div className="w-8 h-8 bg-black rounded-3xl"></div>
+    <p className="text-sm font-semibold mb-2 translate-y-1 ml-2">{name}</p>
+  </div>;
 };
 
 export default CoinInfo;
