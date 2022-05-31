af6aaf0a64a3c0b697abad1bd456127725fc4bf0
Daniel de Nobrega <u20529440@gmail.com>
2022-05-31 22:22:36 +0200

    Updated coin.js and CoinInfo.js

--- client/components/CoinAccount/CoinInfo.js
+++ client/components/CoinAccount/CoinInfo.js
@@ -4,10 +4,10 @@ import { HeartIcon, ChatIcon } from "@heroicons/react/outline";
 const CoinInfo = ({ name, price }) => {
   return (
     <div className="bg-white m-4 p-4 rounded-lg w-full">
-      <div className="flex flew-row items-center mb-2">
-        <div className="w-8 h-8 bg-black rounded-3xl"></div>
-        <p className="text-sm font-semibold mb-2 translate-y-1 ml-2">
-          Hello there
+      <div className="flex flex-col items-center mb-2">
+        <p className="text-sm font-semibold mb-2 translate-y-1 ml-2">{name}</p>
+        <p className="text-3xl font-semibold mb-2 translate-y-1 ml-2">
+          {price}
         </p>
       </div>
     </div>
--- client/pages/coin.js
+++ client/pages/coin.js
@@ -53,7 +53,7 @@ const Coin = () => {
             <p className="text-sm mt-4 text-gray-600">Coin Info</p>
           </div>
           <div className="w-full"></div>
-          <CoinInfo />
+          <CoinInfo name="Sino" price="$100" />
         </div>
       </Layout>
     </>
