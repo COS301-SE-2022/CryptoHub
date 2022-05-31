adb3c84014fbc9251342a00b24d423188719b9c3
Daniel de Nobrega <u20529440@gmail.com>
2022-05-31 19:49:30 +0200

    Added imports to coin.js

--- client/pages/coin.js
+++ client/pages/coin.js
@@ -1,4 +1,11 @@
+import React, { useState, useEffect, useContext } from "react";
 import Layout from "../components/Layout";
+import Head from "next/head";
+import { userContext } from "../auth/auth";
+import Post from "../components/Posts/Post";
+import { useRouter } from "next/router";
+import { XIcon } from "@heroicons/react/outline";
+import SuggestedAccount from "../components/InfoSection/SuggestedAccount";
 
 const Coin = () => {
     return(<>
