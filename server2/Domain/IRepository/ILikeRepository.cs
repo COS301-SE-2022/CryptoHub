3160768fd5c4d529476f4f0d82095a7333bd0de7
Casparus <u20780479@tuks.co.za>
2022-05-31 20:08:08 +0200

    Updated ILikeRepository

--- server2/Domain/IRepository/ILikeRepository.cs
+++ server2/Domain/IRepository/ILikeRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Domain.IRepository
 {
-    internal class Class1
+    public interface ILikeRepository : IAsyncRepository<Like>
     {
+
     }
 }
