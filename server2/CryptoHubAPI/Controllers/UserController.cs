42b04af9b357fa35e98b0cc037ab945a56d4604c
Thabo  Jerome Chesane <u20507102@tuks.co.za>
2022-05-31 19:52:39 +0200

    User ccontroller fixes

--- server2/CryptoHubAPI/Controllers/UserController.cs
+++ server2/CryptoHubAPI/Controllers/UserController.cs
@@ -10,24 +10,24 @@ namespace CryptoHubAPI.Controllers
     public class UserController : Controller
     {
 
-        private readonly IUserRepository userRepository;
+        private readonly IUserRepository _userRepository;
 
         public UserController(IUserRepository userRepository)
         {
-            this.userRepository = userRepository;
+            _userRepository = userRepository;
         }
 
         [HttpGet]
         // GET: UserController
         public async Task<ActionResult<List<User>>> GetAllUsers()
         {
-            return Ok(await userRepository.GetAll());
+            return Ok(await _userRepository.GetAll());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<User>> GetUserById(int id)
         {
-            var response = await userRepository.GetById(u => u.UserId == id);
+            var response = await _userRepository.GetById(u => u.UserId == id);
             if (response == null)
                 return NotFound();
 
@@ -38,7 +38,7 @@ namespace CryptoHubAPI.Controllers
         [HttpGet("{email}")]
         public async Task<ActionResult<User>> GetUserByEmail(string email)
         {
-            var response = await userRepository.FindOne(u => u.Email == email);
+            var response = await _userRepository.FindOne(u => u.Email == email);
             if (response == null)
                 return NotFound();
 
@@ -49,14 +49,14 @@ namespace CryptoHubAPI.Controllers
         [HttpPost]
         public async Task<ActionResult<User>> AddUser([FromBody] User user)
         {
-            return Ok(await userRepository.Add(user));
+            return Ok(await _userRepository.Add(user));
 
         }
 
         [HttpPut]
         public async Task<ActionResult<User>> UpdateUser([FromBody] User user)
         {
-            var response = await userRepository.Update(u => u.UserId == user.UserId, user);
+            var response = await _userRepository.Update(u => u.UserId == user.UserId, user);
             if (response == null)
                 return null;
 
@@ -68,7 +68,7 @@ namespace CryptoHubAPI.Controllers
         [HttpDelete]
         public async Task<IActionResult> Delete(int id)
         {
-            await userRepository.DeleteOne(u => u.UserId == id);
+            await _userRepository.DeleteOne(u => u.UserId == id);
             return Ok();
         }
 
