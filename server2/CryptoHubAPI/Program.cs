012b6779fa578c0c3f7994cea9f193dae5bbcab1
Khotso-Bore <u19180642@tuks.co.za>
2022-05-31 20:28:13 +0200

    DBconnection Changed

--- server2/CryptoHubAPI/Program.cs
+++ server2/CryptoHubAPI/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<CryptoHubDBContext>(
     options =>
     {
-        options.UseSqlServer(builder.Configuration.GetConnectionString("AZSQL"));
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection"));
     });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
