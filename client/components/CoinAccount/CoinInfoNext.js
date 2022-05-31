9f1e78f1ea0846dbcb670aa632b68f7300fa16ee
Daniel de Nobrega <u20529440@gmail.com>
2022-05-31 23:31:01 +0200

    Removed currencies from CoinInfoNext.js

--- client/components/CoinAccount/CoinInfoNext.js
+++ client/components/CoinAccount/CoinInfoNext.js
@@ -10,17 +10,6 @@ const CoinInfoNext = ({ name, price }) => {
           <p className="text-3xl font-semibold mb-2 translate-y-1 ml-2 text-center text-decoration-line: underline">
             {name}
           </p>
-          <div className="flex flex-row justify-between">
-            <button className="text-xl font-semibold mb-2 translate-y-1 ml-2 text-right border-black border-2 p-1 rounded-md">
-              Rand
-            </button>
-            <button className="text-xl font-semibold mb-2 translate-y-1 ml-2 text-right border-black border-2 p-1 rounded-md">
-              USD
-            </button>
-            <button className="text-xl font-semibold mb-2 translate-y-1 ml-2 text-right border-black border-2 p-1 rounded-md">
-              EUR
-            </button>
-          </div>
         </div>
       </div>
     </div>
