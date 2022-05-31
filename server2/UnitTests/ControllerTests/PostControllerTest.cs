403e304be2f3dce3da12103285a65423312c8561
Casparus <u20780479@tuks.co.za>
2022-05-31 23:13:44 +0200

    Created PostControllerTest

---
+++ server2/UnitTests/ControllerTests/PostControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using CryptoHubAPI.Controllers;
+using Domain.IRepository;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Linq.Expressions;
+
+namespace UnitTests.ControllerTests
+{
+    public class PostControllerTest
+    {
+        private readonly Mock<IPostRepository> _postRepositoryMock;
+    }
+}
