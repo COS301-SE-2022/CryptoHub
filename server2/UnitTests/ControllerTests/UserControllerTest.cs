9bba7f38fc23ae2fd175b1034bd961e9cdf2a344
Casparus <u20780479@tuks.co.za>
2022-05-31 22:48:13 +0200

    Added GetUserByEmail test

--- server2/UnitTests/ControllerTests/UserControllerTest.cs
+++ server2/UnitTests/ControllerTests/UserControllerTest.cs
@@ -129,5 +129,63 @@ namespace UnitTests.ControllerTests
             var actual = (result.Result as OkObjectResult).Value;
             Assert.IsType<User>(actual);
         }
+
+        [Fact]
+        public async Task GetUserByEmail_UserEmail_ReturnsUserOfEmail()
+        {
+            //arrange
+            List<User> users = new List<User>
+            {
+                new User
+                {
+                    UserId = 1,
+                    Email = "johndoe@gmail.com",
+                    Firstname = "john",
+                    Lastname = "doe",
+                    Username = "john",
+                    Password = "1234"
+                },
+                new User
+                {
+                    UserId = 2,
+                    Email = "elonmusk@gmail.com",
+                    Firstname = "elon",
+                    Lastname = "musk",
+                    Username = "elon",
+                    Password = "1234"
+                },
+                new User
+                {
+                    UserId = 3,
+                    Email = "billgates@gmail.com",
+                    Firstname = "bill",
+                    Lastname = "gates",
+                    Username = "bill",
+                    Password = "windows"
+                }
+            };
+            var user = new User
+            {
+                UserId = 1,
+                Email = "johndoe@gmail.com",
+                Firstname = "john",
+                Lastname = "doe",
+                Username = "john",
+                Password = "1234"
+            };
+
+            _userRepositoryMock.Setup(u => u.FindOne(It.IsAny<Expression<Func<User, bool>>>())).ReturnsAsync(user);
+
+            var controller = new UserController(_userRepositoryMock.Object);
+
+            //act
+            var result = await controller.GetUserByEmail(user.Email);
+
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result.Result);
+
+            var actual = (result.Result as OkObjectResult).Value;
+            Assert.IsType<User>(actual);
+        }
     }
 }
