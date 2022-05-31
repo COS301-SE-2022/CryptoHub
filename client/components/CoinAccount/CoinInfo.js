833ce43683674f1ff0fb79dca0a5e63d5329baa7
Daniel de Nobrega <u20529440@gmail.com>
2022-05-31 22:53:27 +0200

    Made changes to the coin.js and CoinInfo.js

--- client/components/CoinAccount/CoinInfo.js
+++ client/components/CoinAccount/CoinInfo.js
@@ -6,17 +6,17 @@ const CoinInfo = ({ name, price }) => {
     <div className="bg-white m-4 p-4 rounded-lg w-full">
       <div className="flex flex-col mb-2">
         <div className="flex flex-row justify-between">
-          <p className="text-xl font-semibold mb-2 translate-y-1 ml-2 text-center">
+          <p className="text-3xl font-semibold mb-2 translate-y-1 ml-2 text-center text-decoration-line: underline">
             {name}
           </p>
           <div className="flex flex-row justify-between">
-            <p className="text-xl font-semibold mb-2 translate-y-1 ml-2 text-right border-gray-100 border-2 p-1 rounded-md">
+            <p className="text-xl font-semibold mb-2 translate-y-1 ml-2 text-right border-black border-2 p-1 rounded-md">
               Rand
             </p>
-            <p className="text-xl font-semibold mb-2 translate-y-1 ml-2 text-right border-gray-100 border-2 p-1 rounded-md">
+            <p className="text-xl font-semibold mb-2 translate-y-1 ml-2 text-right border-black border-2 p-1 rounded-md">
               USD
             </p>
-            <p className="text-xl font-semibold mb-2 translate-y-1 ml-2 text-right border-gray-100 border-2 p-1 rounded-md">
+            <p className="text-xl font-semibold mb-2 translate-y-1 ml-2 text-right border-black border-2 p-1 rounded-md">
               UAE
             </p>
           </div>
