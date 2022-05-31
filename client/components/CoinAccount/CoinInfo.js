865cacac00672f56ff55f166b2fff40b14f18a98
Daniel de Nobrega <u20529440@gmail.com>
2022-05-31 22:37:51 +0200

    Updated CoinInfo.js to allow changing between currency

--- client/components/CoinAccount/CoinInfo.js
+++ client/components/CoinAccount/CoinInfo.js
@@ -4,11 +4,25 @@ import { HeartIcon, ChatIcon } from "@heroicons/react/outline";
 const CoinInfo = ({ name, price }) => {
   return (
     <div className="bg-white m-4 p-4 rounded-lg w-full">
-      <div className="flex flex-col items-center mb-2">
-        <p className="text-sm font-semibold mb-2 translate-y-1 ml-2">{name}</p>
-        <p className="text-3xl font-semibold mb-2 translate-y-1 ml-2">
-          {price}
-        </p>
+      <div className="flex flex-col mb-2">
+        <div className="flex flex-row justify-between">
+          <p className="text-xl font-semibold mb-2 translate-y-1 ml-2 text-center">
+            {name}
+          </p>
+          <div className="flex flex-row justify-between">
+            <p className="text-xl font-semibold mb-2 translate-y-1 ml-2 text-right">
+              Rand
+            </p>
+            <p className="text-xl font-semibold mb-2 translate-y-1 ml-2 text-right">
+              USD
+            </p>
+            <p className="text-xl font-semibold mb-2 translate-y-1 ml-2 text-right">
+              UAE
+            </p>
+          </div>
+        </div>
+
+        <p className="text-3xl font-bold mb-2 translate-y-1 ml-2">{price}</p>
       </div>
     </div>
   );
