3e9aa1804fe55aab67fe47ad42b0de315b8ae45f
Casparus <u20780479@tuks.co.za>
2022-05-31 19:40:03 +0200

    Added AddLike method

--- server2/CryptoHubAPI/Controllers/LikeController.cs
+++ server2/CryptoHubAPI/Controllers/LikeController.cs
@@ -1,6 +1,25 @@
-﻿namespace CryptoHubAPI.Controllers
+﻿using Domain.IRepository;
+using Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoHubAPI.Controllers
 {
-    public class Class
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class LikeController : Controller
     {
+        private readonly ILikeRepository likeRepository;
+
+        public LikeController(ILikeRepository likeRepository)
+        {
+            this.likeRepository = likeRepository;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Like>> AddLike([FromBody] Like Like)
+        {
+            return Ok(await likeRepository.Add(Like));
+        }
     }
 }
