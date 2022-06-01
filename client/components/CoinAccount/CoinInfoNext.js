232a9f009e08e91c7abf3799912498558f791577
Daniel de Nobrega <u20529440@gmail.com>
2022-06-01 20:09:29 +0200

    removed currency types from CoinInfoNext.js

--- client/components/CoinAccount/CoinInfoNext.js
+++ client/components/CoinAccount/CoinInfoNext.js
@@ -10,22 +10,12 @@ const CoinInfoNext = ({ name, state }) => {
           <p className="text-3xl font-semibold mb-2 translate-y-1 ml-2 text-center text-decoration-line: underline">
             {name}
           </p>
-          <div className="flex flex-row justify-between">
-            <button className="text-xs font-semibold mb-2 translate-y-1 ml-2 text-right border-black border-2 p-1 rounded-md">
-              1 day
-            </button>
-            <button className="text-xs font-semibold mb-2 translate-y-1 ml-2 text-right border-black border-2 p-1 rounded-md">
-              1 week
-            </button>
-            <button className="text-xs font-semibold mb-2 translate-y-1 ml-2 text-right border-black border-2 p-1 rounded-md">
-              1 month
-            </button>
-          </div>
         </div>
         <div className="flex flex-row">
-          <p className="text-5xl font-bold mb-2 translate-y-1 ml-2 justify-between">
+          <p className="text-3xl font-bold mb-2 translate-y-1 ml-2 justify-between">
             {state}
           </p>
+          <div class="h-0 w-4 border-x-8 border-x-transparent border-b-[16px] border-b-green-600"></div>
         </div>
       </div>
     </div>
