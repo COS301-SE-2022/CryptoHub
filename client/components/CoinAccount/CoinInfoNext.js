e9a53aa21e63a2c8fbb485fe2b7abf1845e036d5
Daniel de Nobrega <u20529440@gmail.com>
2022-06-01 19:43:04 +0200

    added the percentage at which the coin has increased or decreased to CoinInfoNext.js

--- client/components/CoinAccount/CoinInfoNext.js
+++ client/components/CoinAccount/CoinInfoNext.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { HeartIcon, ChatIcon } from "@heroicons/react/outline";
 
-const CoinInfoNext = ({ name, price }) => {
+const CoinInfoNext = ({ name, state }) => {
   return (
     <div className="bg-white m-4 p-4 rounded-lg w-full">
       <div className="flex flex-col mb-2">
@@ -11,16 +11,21 @@ const CoinInfoNext = ({ name, price }) => {
             {name}
           </p>
           <div className="flex flex-row justify-between">
-            <button className="text-xl font-semibold mb-2 translate-y-1 ml-2 text-right border-black border-2 p-1 rounded-md">
+            <button className="text-xs font-semibold mb-2 translate-y-1 ml-2 text-right border-black border-2 p-1 rounded-md">
               1 day
             </button>
-            <button className="text-xl font-semibold mb-2 translate-y-1 ml-2 text-right border-black border-2 p-1 rounded-md">
+            <button className="text-xs font-semibold mb-2 translate-y-1 ml-2 text-right border-black border-2 p-1 rounded-md">
               1 week
             </button>
-            <button className="text-xl font-semibold mb-2 translate-y-1 ml-2 text-right border-black border-2 p-1 rounded-md">
+            <button className="text-xs font-semibold mb-2 translate-y-1 ml-2 text-right border-black border-2 p-1 rounded-md">
               1 month
             </button>
           </div>
+        </div>
+        <div className="flex flex-row">
+          <p className="text-5xl font-bold mb-2 translate-y-1 ml-2 justify-between">
+            {state}
+          </p>
         </div>
       </div>
     </div>
--- client/pages/coin.js
+++ client/pages/coin.js
@@ -55,7 +55,7 @@ const Coin = () => {
           </div>
           <div className="w-full"></div>
           <CoinInfo name="Current Price: " price="100" />
-          <CoinInfoNext name="Current State: " price="100" />
+          <CoinInfoNext name="Current State: " state="13.56%" />
         </div>
       </Layout>
     </>
