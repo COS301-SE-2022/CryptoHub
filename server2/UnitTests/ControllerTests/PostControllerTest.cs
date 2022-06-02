c189ed72c3c7d38d5c9b559df45b4fcfc0784e08
Casparus <u20780479@tuks.co.za>
2022-06-02 20:58:46 +0200

    Added GetPostByUserId Integration test

--- server2/UnitTests/ControllerTests/PostControllerTest.cs
+++ server2/UnitTests/ControllerTests/PostControllerTest.cs
@@ -15,5 +15,52 @@ namespace UnitTests.ControllerTests
         {
             _postRepositoryMock = new Mock<IPostRepository>();
         }
+        [Fact]
+        public async Task GetPostByUserId_ListOfPosts_ReturnsListOfPosts()
+        {
+            //arrange
+            List<Post> users = new List<Post>
+                {
+                    new Post
+                    {
+                        PostId = 1,
+                        Post1 = "Post 1",
+                        UserId = 1
+                    },
+                    new Post
+                    {
+                        PostId = 2,
+                        Post1 = "Post 2",
+                        UserId = 2
+                    },
+                    new Post
+                    {
+                        PostId = 3,
+                        Post1 = "Post 3",
+                        UserId = 3
+                    }
+                };
+
+            var post = new Post
+            {
+                PostId = 1,
+                Post1 = "Post 1",
+                UserId = 1
+            };
+
+            _postRepositoryMock.Setup(x => x.FindOne(It.IsAny<Expression<Func<Post, bool>>>())).ReturnsAsync(post);
+
+            var controller = new PostController(_postRepositoryMock.Object);
+
+            //act
+            var result = await controller.GetPostByUserId(1);
+
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result.Result);
+
+            var actual = (result.Result as OkObjectResult).Value;
+            Assert.IsType<User>(actual);
+        }
+
     }
 }
