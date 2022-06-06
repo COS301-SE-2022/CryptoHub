c865ce013fc0ba4c40932132f5f7bb2c8f63ee4f
Casparus <u20780479@tuks.co.za>
2022-06-06 20:02:20 +0200

    Updated GetPostByUserId test

--- server2/UnitTests/ControllerTests/PostControllerTest.cs
+++ server2/UnitTests/ControllerTests/PostControllerTest.cs
@@ -20,26 +20,26 @@ namespace UnitTests.ControllerTests
         {
             //arrange
             List<Post> posts = new List<Post>
+            {
+                new Post
                 {
-                    new Post
-                    {
-                        PostId = 1,
-                        Post1 = "Post 1",
-                        UserId = 1
-                    },
-                    new Post
-                    {
-                        PostId = 2,
-                        Post1 = "Post 2",
-                        UserId = 2
-                    },
-                    new Post
-                    {
-                        PostId = 3,
-                        Post1 = "Post 3",
-                        UserId = 3
-                    }
-                };
+                    PostId = 1,
+                    Post1 = "Post 1",
+                    UserId = 1
+                },
+                new Post
+                {
+                    PostId = 2,
+                    Post1 = "Post 2",
+                    UserId = 2
+                },
+                new Post
+                {
+                    PostId = 3,
+                    Post1 = "Post 3",
+                    UserId = 3
+                }
+            };
 
             _postRepositoryMock.Setup(u => u.GetAll()).ReturnsAsync(posts);
 
@@ -51,10 +51,46 @@ namespace UnitTests.ControllerTests
             Assert.NotNull(result);
             Assert.IsType<OkObjectResult>(result.Result);
 
-
             var actual = (result.Result as OkObjectResult).Value;
             Assert.IsType<List<Post>>(actual);
             Assert.Equal(3, (actual as List<Post>).Count);
         }
+
+        [Fact]
+        public async Task GetPostsByUserId_UserId_ReturnsPostsOfId()
+        {
+            //arrange
+            List<Post> posts = new List<Post>
+            {
+                new Post
+                {
+                    PostId = 1,
+                    Post1 = "Post 1",
+                    UserId = 1
+                },
+                new Post
+                {
+                    PostId = 2,
+                    Post1 = "Post 2",
+                    UserId = 2
+                },
+                new Post
+                {
+                    PostId = 3,
+                    Post1 = "Post 3",
+                    UserId = 3
+                }
+            };
+
+            var post = new Post
+            {
+                PostId = 1,
+                Post1 = "Post 1",
+                UserId = 1
+            };
+
+
+        }
+
     }
 }
