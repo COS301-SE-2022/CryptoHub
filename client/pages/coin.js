a71df1fff3dd2d4d6567538c02c2df4d1976f143
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2022-06-06 11:21:31 +0200

    updated coin page styling

--- client/pages/coin.js
+++ client/pages/coin.js
@@ -54,8 +54,8 @@ const Coin = () => {
             <p className="text-sm mt-4 text-gray-600">Coin Info</p>
           </div>
           <div className="w-full"></div>
-          <CoinInfo name="Current Price: " price="100" />
-          <CoinInfoNext name="Current State: " state="13.56%" arrow="up" />
+          <CoinInfo name="Current Price" price="100" />
+          <CoinInfoNext name="Current State" state="13.56%" arrow="up" />
         </div>
       </Layout>
     </>
