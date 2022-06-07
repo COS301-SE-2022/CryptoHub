cb65c60dfe4896570055fceb467876746bbe18fb
Casparus <u20780479@tuks.co.za>
2022-06-07 23:03:24 +0200

    Finished CommentController tests

--- server2/UnitTests/ControllerTests/CommentControllerTest.cs
+++ server2/UnitTests/ControllerTests/CommentControllerTest.cs
@@ -17,7 +17,7 @@ namespace UnitTests.ControllerTests
         }
 
         [Fact]
-        public async Task GetCommentsByUserId_UserId_ReturnsCommentsOfId()
+        public async Task GetCommentsByCommentId_CommentId_ReturnsCommentsOfId()
         {
             //arrange
             List<Comment> comments = new List<Comment>
@@ -44,6 +44,107 @@ namespace UnitTests.ControllerTests
             var actual = (result.Result as OkObjectResult).Value;
             Assert.IsType<List<Comment>>(actual);
             Assert.Equal(1, (actual as List<Comment>).Count);
+        }
+
+        [Fact]
+        public async Task GetCommentsByPostId_PostId_ReturnsCommentsOfId()
+        {
+            //arrange
+            List<Comment> comments = new List<Comment>
+            {
+                new Comment
+                {
+                    CommentId = 1,
+                    UserId = 1,
+                    PostId = 1,
+                    Comment1 = "CommentText"
+                }
+            };
+
+            _commentRepositoryMock.Setup(u => u.FindRange(It.IsAny<Expression<Func<Comment, bool>>>())).ReturnsAsync(comments);
+
+            var controller = new CommentController(_commentRepositoryMock.Object);
+
+            //act
+            var result = await controller.GetCommentByUserId(1);
+
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result.Result);
+
+            var actual = (result.Result as OkObjectResult).Value;
+            Assert.IsType<List<Comment>>(actual);
+            Assert.Equal(1, (actual as List<Comment>).Count);
+        }
+
+        [Fact]
+        public async Task AddComment_Comment_ReturnsComment()
+        {
+            //arrange
+            var comment = new Comment
+            {
+                CommentId = 1,
+                UserId = 1,
+                PostId = 1,
+                Comment1 = "CommentText"
+            };
+            _commentRepositoryMock.Setup(u => u.Add(It.IsAny<Comment>())).ReturnsAsync(comment);
+
+            var controller = new CommentController(_commentRepositoryMock.Object);
+
+            //act
+            var result = await controller.AddComment(comment);
+
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result.Result);
+
+            var actual = (result.Result as OkObjectResult).Value;
+            Assert.IsType<Comment>(actual);
+        }
+
+        [Fact]
+        public async Task UpdateComment_Comment_ReturnsComment()
+        {
+            //arrange
+            var comment = new Comment
+            {
+                CommentId = 1,
+                UserId = 1,
+                PostId = 1,
+                Comment1 = "CommentText"
+            };
+
+            _commentRepositoryMock.Setup(u => u.Update(It.IsAny<Expression<Func<Comment, bool>>>(), It.IsAny<Comment>())).ReturnsAsync(comment);
+
+            var controller = new CommentController(_commentRepositoryMock.Object);
+
+            //act
+            var result = await controller.UpdateComment(comment);
+
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result.Result);
+
+            var actual = (result.Result as OkObjectResult).Value;
+            Assert.IsType<Comment>(actual);
+        }
+
+        [Fact]
+        public async Task Delete_Comment_ReturnsNone()
+        {
+            //arrange
+            var comment = new Comment
+            {
+                CommentId = 1,
+                UserId = 1,
+                PostId = 1,
+                Comment1 = "CommentText"
+            };
+
+            _commentRepositoryMock.Setup(u => u.DeleteOne(It.IsAny<Expression<Func<Comment, bool>>>()));
+
+            var controller = new CommentController(_commentRepositoryMock.Object);
+
+            //act
+            var result = await controller.Delete(comment.CommentId);
         }
     }
 }
