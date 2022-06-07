8cb6e93be28e072ba543f5ab04111a84fa43e6e7
Casparus <u20780479@tuks.co.za>
2022-06-07 22:13:11 +0200

    Finished AuthorizationController tests

--- server2/UnitTests/ControllerTests/AuthorizationControllerTest.cs
+++ server2/UnitTests/ControllerTests/AuthorizationControllerTest.cs
@@ -20,7 +20,6 @@ namespace UnitTests.ControllerTests
         public async Task Login_User_ReturnsUser()
         {
             //arrange
-
             var user = new User
             {
                 UserId = 1,
@@ -49,6 +48,46 @@ namespace UnitTests.ControllerTests
 
             //act2
             var result2 = await controllerAuth.Login(user);
+
+            Assert.NotNull(result2);
+            Assert.IsType<BadRequestObjectResult>(result2.Result);
+
+            var actual2 = (result2.Result as BadRequestObjectResult).Value;
+            Assert.IsType<Response<User>>(actual2);
+        }
+
+        [Fact]
+        public async Task Register_User_ReturnsUser()
+        {
+            //arrange
+            var user = new User
+            {
+                UserId = 1,
+                Email = "johndoe@gmail.com",
+                Firstname = "john",
+                Lastname = "doe",
+                Username = "john",
+                Password = "1234"
+            };
+
+            _userRepositoryMock.Setup(u => u.FindOne(It.IsAny<Expression<Func<User, bool>>>())).ReturnsAsync((User)null);
+
+            var controllerAuth = new AuthorizationController(_userRepositoryMock.Object);
+
+            //act
+            var result = await controllerAuth.Register(user);
+
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result.Result);
+
+            var actual = (result.Result as OkObjectResult).Value;
+            Assert.IsType<Response<User>>(actual);
+
+            //arrange 2 Tests for null return (user already registered)
+            _userRepositoryMock.Setup(u => u.FindOne(It.IsAny<Expression<Func<User, bool>>>())).ReturnsAsync(user);
+
+            //act2
+            var result2 = await controllerAuth.Register(user);
 
             Assert.NotNull(result2);
             Assert.IsType<BadRequestObjectResult>(result2.Result);
