5674f5ede131cd156bb8ec1d8ea829af8bf8e734
Thabo  Jerome Chesane <u20507102@tuks.co.za>
2022-06-07 22:05:59 +0200

    Image repository

---
+++ server2/Infrastructure/Repository/ImageRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Domain.Infrastructure;
+
+namespace Infrastructure.Repository
+{
+	public class ImageRepository : BaseRepository<Image>, IImageRepository
+	{
+		public ImageRepository(CryptoHubDBContext dBContext) : base(dBContext)
+		{
+
+		}
+	}
+}
+
