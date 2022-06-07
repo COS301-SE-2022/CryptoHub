812572226f211f392d95d3a1b113b7e894a1195f
Casparus <u20780479@tuks.co.za>
2022-06-07 23:31:49 +0200

    Finished LikeControllerTest

--- server2/UnitTests/ControllerTests/LikeControllerTest.cs
+++ server2/UnitTests/ControllerTests/LikeControllerTest.cs
@@ -57,8 +57,161 @@ namespace UnitTests.ControllerTests
             var actual = (result.Result as OkObjectResult).Value;
             Assert.IsType<List<Like>>(actual);
             Assert.Equal(3, (actual as List<Like>).Count);
+        }
 
+        [Fact]
+        public async Task GetLikesByUserId_UserId_ReturnsLikesOfId()
+        {
+            //arrange
+            List<Like> likes = new List<Like>
+            {
+                new Like
+                {
+                    LikeId = 1,
+                    UserId = 1,
+                    PostId = 1,
+                }
+            };
 
+            _likeRepositoryMock.Setup(u => u.FindRange(It.IsAny<Expression<Func<Like, bool>>>())).ReturnsAsync(likes);
+
+            var controller = new LikeController(_likeRepositoryMock.Object);
+
+            //act
+            var result = await controller.GetLikeByUserId(1);
+
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result.Result);
+
+            var actual = (result.Result as OkObjectResult).Value;
+            Assert.IsType<List<Like>>(actual);
+            Assert.Equal(1, (actual as List<Like>).Count);
+        }
+
+        [Fact]
+        public async Task GetLikesByPostId_PostId_ReturnsLikesOfId()
+        {
+            //arrange
+            List<Like> likes = new List<Like>
+            {
+                new Like
+                {
+                    LikeId = 1,
+                    UserId = 1,
+                    PostId = 1,
+                }
+            };
+
+            _likeRepositoryMock.Setup(u => u.FindRange(It.IsAny<Expression<Func<Like, bool>>>())).ReturnsAsync(likes);
+
+            var controller = new LikeController(_likeRepositoryMock.Object);
+
+            //act
+            var result = await controller.GetLikeByPostId(1);
+
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result.Result);
+
+            var actual = (result.Result as OkObjectResult).Value;
+            Assert.IsType<List<Like>>(actual);
+            Assert.Equal(1, (actual as List<Like>).Count);
+        }
+
+        [Fact]
+        public async Task GetLikesByLikeId_LikeId_ReturnsLikesOfId()
+        {
+            //arrange
+            List<Like> likes = new List<Like>
+            {
+                new Like
+                {
+                    LikeId = 1,
+                    UserId = 1,
+                    PostId = 1,
+                }
+            };
+
+            _likeRepositoryMock.Setup(u => u.FindRange(It.IsAny<Expression<Func<Like, bool>>>())).ReturnsAsync(likes);
+
+            var controller = new LikeController(_likeRepositoryMock.Object);
+
+            //act
+            var result = await controller.GetLikeByPostId(1);
+
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result.Result);
+
+            var actual = (result.Result as OkObjectResult).Value;
+            Assert.IsType<List<Like>>(actual);
+            Assert.Equal(1, (actual as List<Like>).Count);
+        }
+
+        [Fact]
+        public async Task AddLike_Like_ReturnsLike()
+        {
+            //arrange
+            var like = new Like
+            {
+                LikeId = 1,
+                UserId = 1,
+                PostId = 1,
+            };
+            _likeRepositoryMock.Setup(u => u.Add(It.IsAny<Like>())).ReturnsAsync(like);
+
+            var controller = new LikeController(_likeRepositoryMock.Object);
+
+            //act
+            var result = await controller.AddLike(like);
+
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result.Result);
+
+            var actual = (result.Result as OkObjectResult).Value;
+            Assert.IsType<Like>(actual);
+        }
+
+        [Fact]
+        public async Task UpdateLike_Like_ReturnsLike()
+        {
+            //arrange
+            var like = new Like
+            {
+                LikeId = 1,
+                UserId = 1,
+                PostId = 1,
+            };
+
+            _likeRepositoryMock.Setup(u => u.Update(It.IsAny<Expression<Func<Like, bool>>>(), It.IsAny<Like>())).ReturnsAsync(like);
+
+            var controller = new LikeController(_likeRepositoryMock.Object);
+
+            //act
+            var result = await controller.UpdateLike(like);
+
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result.Result);
+
+            var actual = (result.Result as OkObjectResult).Value;
+            Assert.IsType<Like>(actual);
+        }
+
+        [Fact]
+        public async Task Delete_Like_None()
+        {
+            //arrange
+            var like = new Like
+            {
+                LikeId = 1,
+                UserId = 1,
+                PostId = 1,
+            };
+
+            _likeRepositoryMock.Setup(u => u.DeleteOne(It.IsAny<Expression<Func<Like, bool>>>()));
+
+            var controller = new LikeController(_likeRepositoryMock.Object);
+
+            //act
+            var result = await controller.Delete(like.LikeId);
         }
     }
 }
