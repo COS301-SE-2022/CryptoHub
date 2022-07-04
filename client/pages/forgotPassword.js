ebc7b725e26ba92e9c52a4a7fa228feebbdd3e40
Daniel de Nobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-07-04 19:33:17 +0200

    Added imports to forgotPassword.js

--- client/pages/forgotPassword.js
+++ client/pages/forgotPassword.js
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { useState } from "react";
+import { LockClosedIcon } from "@heroicons/react/solid";
+import { userContext } from "../auth/auth";
+import { useContext } from "react";
+import { useRouter } from "next/router"
 
 function forgotPassword() {
 
