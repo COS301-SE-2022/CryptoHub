098725a4c033ed6d696b715542e2630ddbf8409e
Daniel de Nobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-07-04 18:38:37 +0200

    Update CoinInfo.js

--- client/components/CoinAccount/CoinInfo.js
+++ client/components/CoinAccount/CoinInfo.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 
 const CoinInfo = ({ name, price }) => {
   const [amount, setAmount] = useState(0)
+  const [Zar, setZAR] = useState(0)
+  const [USD, setUSD] = useState(0)
+  const [EUR, setEUR] = useState(0)
+
 
   const handleCurrencyConversion = (have, want, amount) => {
     const options = {
@@ -15,14 +19,10 @@ const CoinInfo = ({ name, price }) => {
     .then((response) => response.json())
     .then((data) => {
       setAmount(data.new_amount)
-      console.warn(data);
+      console.log(data);
     })
     .catch(() => {})
   };
-  
-  useEffect(() => {
-    handleCurrencyConversion("ZAR","USD","1000");
-  }, []);
 
   return (
     <div className="bg-white m-4 p-4 rounded-lg w-full">
@@ -32,13 +32,13 @@ const CoinInfo = ({ name, price }) => {
             {name}
           </p>
           <div className="flex flex-row justify-between">
-            <button className="text-sm font-semibold mb-2 translate-y-1 ml-1 text-right p-1 px-3 rounded-md bg-gray-100 hover:bg-indigo-300 transition">
+            <button onClick={handleCurrencyConversion("USD","ZAR",price)} className="text-sm font-semibold mb-2 translate-y-1 ml-1 text-right p-1 px-3 rounded-md bg-gray-100 hover:bg-indigo-300 transition">
               ZAR
             </button>
-            <button className="text-sm font-semibold mb-2 translate-y-1 ml-1 text-right p-1 px-3 rounded-md bg-gray-100 hover:bg-indigo-300 transition">
+            <button onClick={handleCurrencyConversion("USD","USD",price)} className="text-sm font-semibold mb-2 translate-y-1 ml-1 text-right p-1 px-3 rounded-md bg-gray-100 hover:bg-indigo-300 transition">
               USD
             </button>
-            <button className="text-sm font-semibold mb-2 translate-y-1 ml-1 text-right p-1 px-3 rounded-md bg-gray-100 hover:bg-indigo-300 transition">
+            <button onClick={handleCurrencyConversion("USD","EUR",price)} className="text-sm font-semibold mb-2 translate-y-1 ml-1 text-right p-1 px-3 rounded-md bg-gray-100 hover:bg-indigo-300 transition">
               EUR
             </button>
           </div>
@@ -46,7 +46,7 @@ const CoinInfo = ({ name, price }) => {
 
         <div className="flex flex-row">
           <p className="text-6xl font-bold mb-2 translate-y-1 ml-2 justify-between">
-            {price}
+            {amount}
           </p>
           <p className="text-sm font-semibold ml-1 translate-y-3 text-gray-400 text-left items-center">
             USD
