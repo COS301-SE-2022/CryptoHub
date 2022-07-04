7586f706aa752b1ab26e603bbca46a737f6fd43a
Daniel de Nobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-07-04 20:27:41 +0200

    Update forgotPassword.js

--- client/pages/forgotPassword.js
+++ client/pages/forgotPassword.js
@@ -74,6 +74,19 @@ function forgotPassword() {
                   </div>
                 </div>
 
+                <div>
+                  <button
+                    type="submit"
+                    className="group relative flex justify-center py-2 px-4 border border-transparent text-lg font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    {loading ? (
+                      <p className="text-indigo-200">Loading...</p>
+                    ) : (
+                      <p>Create Account</p>
+                    )}
+                  </button>
+                </div>
+
               </form>
               {/* {error ? (
                 <h2 className="text-center text-sm font-semibold text-red-500">
