34e67e6fca9b6d0f974605e3c08cf8aa34d5aca8
Daniel de Nobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-07-04 20:48:52 +0200

    Update forgotPassword.js

--- client/pages/forgotPassword.js
+++ client/pages/forgotPassword.js
@@ -87,18 +87,16 @@ function forgotPassword() {
                   </button>
                 </div>
 
-                <div>
-                  <button
-                    type="submit"
-                    className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  >
-                    {loading ? (
-                      <p className="text-indigo-200">Loading...</p>
-                    ) : (
-                      <p className = "justify-center">Back to Lgin Page</p>
-                    )}
-                  </button>
-                </div>
+                <div className="flex items-center justify-center">
+              <div className="text-sm">
+                <a
+                  href="/login"
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                >
+                  Back to Login
+                </a>
+              </div>
+            </div>
 
 
               </form>
