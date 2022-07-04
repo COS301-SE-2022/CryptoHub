f468a7b8753cb934c43769b634cd794227ccdd26
Daniel de Nobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-07-04 21:30:16 +0200

    Update forgotPasswordConfirmation.js

--- client/pages/forgotPasswordConfirmation.js
+++ client/pages/forgotPasswordConfirmation.js
@@ -5,6 +5,13 @@ import { useContext } from "react";
 import { useRouter } from "next/router"
 
 function forgotPasswordConfirmation() {
+    const { authorise } = useContext(userContext);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const router = useRouter();
+    
   return (
     <div>forgotPasswordConfirmation</div>
   )
