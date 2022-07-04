e1acedbc3a9a80031546168b589c91c91e399653
Daniel de Nobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-07-04 21:40:17 +0200

    Update forgotPasswordConfirmation.js

--- client/pages/forgotPasswordConfirmation.js
+++ client/pages/forgotPasswordConfirmation.js
@@ -11,9 +11,72 @@ function forgotPasswordConfirmation() {
     const [error, setError] = useState(false);
     const [loading, setLoading] = useState(false);
     const router = useRouter();
-    
+
   return (
-    <div>forgotPasswordConfirmation</div>
+    <>
+          <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <div className="max-w-md w-full space-y-8">
+              <div>
+                <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                  Confirm it is YOU
+                </h2>
+              </div>
+              <form className="mt-8 space-y-6" >
+                <input type="hidden" name="remember" defaultValue="true" />
+                <div className="rounded-md shadow-sm -space-y-px">
+                  <div>
+                    <label htmlFor="email-address" className="sr-only">
+                      Email address
+                    </label>
+                    <input
+                      id="email-address"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                      placeholder="Email address"
+                      onChange={(e) => {
+                        setEmail(e.target.value);
+                      }}
+                    />
+                  </div>
+                </div>
+    
+                <div>
+                  <button
+                    type="submit"
+                    className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    {loading ? (
+                      <p className="text-indigo-200">Loading...</p>
+                    ) : (
+                      <a href="/forgotPasswordConfirmation">Confirm</a>
+                    )}
+                  </button>
+                </div>
+
+                <div className="flex items-center justify-center">
+              <div className="text-sm">
+                <a
+                  href="/login"
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                >
+                  Back to Login
+                </a>
+              </div>
+            </div>
+
+
+              </form>
+              {error ? (
+                <h2 className="text-center text-sm font-semibold text-red-500">
+                  invalid login credentials
+                </h2>
+              ) : null}
+            </div>
+          </div>
+        </>
   )
 }
 
