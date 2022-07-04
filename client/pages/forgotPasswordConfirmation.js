f8b20f19fe95676ca0ec1c289a1a22a80be5c19a
Daniel de Nobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-07-04 21:29:48 +0200

    Update forgotPasswordConfirmation.js

--- client/pages/forgotPasswordConfirmation.js
+++ client/pages/forgotPasswordConfirmation.js
@@ -1,3 +1,9 @@
+import React, { useState } from "react";
+import { LockClosedIcon } from "@heroicons/react/solid";
+import { userContext } from "../auth/auth";
+import { useContext } from "react";
+import { useRouter } from "next/router"
+
 function forgotPasswordConfirmation() {
   return (
     <div>forgotPasswordConfirmation</div>
