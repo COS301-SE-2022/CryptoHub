0e18949b51d01b0aec259b74bd29d099cbedc3f0
Daniel de Nobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-07-04 19:50:02 +0200

    Update forgotPassword.js

--- client/pages/forgotPassword.js
+++ client/pages/forgotPassword.js
@@ -11,10 +11,83 @@ function forgotPassword() {
     const [error, setError] = useState(false);
     const [loading, setLoading] = useState(false);
     const router = useRouter();
-  return (
+
+    return (
+        <>
+          <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <div className="max-w-md w-full space-y-8">
+              <div>
+                <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                  Trouble Logging in?
+                </h2>
+              </div>
+              <form className="mt-8 space-y-6" >
+                <input type="hidden" name="remember" defaultValue="true" />
+                <div className="rounded-md shadow-sm -space-y-px">
+                  <div>
+                    <label htmlFor="email-address" className="sr-only">
+                      Email address
+                    </label>
+                    <input
+                      id="email-address"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                      placeholder="Email address"
+                      onChange={(e) => {
+                        setEmail(e.target.value);
+                      }}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="password" className="sr-only">
+                      Password
+                    </label>
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                      placeholder="Password"
+                      onChange={(e) => {
+                        setPassword(e.target.value);
+                      }}
+                    />
+                  </div>
+                </div>
     
-    <div>forgotPassword</div>
-  )
+                <div>
+                  <button
+                    type="submit"
+                    className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                      <LockClosedIcon
+                        className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                        aria-hidden="true"
+                      />
+                    </span>
+                    {loading ? (
+                      <p className="text-indigo-200">Loading...</p>
+                    ) : (
+                      <p>Log in</p>
+                    )}
+                  </button>
+                </div>
+              </form>
+              {error ? (
+                <h2 className="text-center text-sm font-semibold text-red-500">
+                  invalid login credentials
+                </h2>
+              ) : null}
+            </div>
+          </div>
+        </>
+      );
 }
 
 export default forgotPassword
