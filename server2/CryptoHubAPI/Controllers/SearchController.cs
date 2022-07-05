920f68dac4344332dcb7384b49db28e03be96028
Thabo  Jerome Chesane <u20507102@tuks.co.za>
2022-07-05 12:40:24 +0200

    Update SearchController.cs

--- server2/CryptoHubAPI/Controllers/SearchController.cs
+++ server2/CryptoHubAPI/Controllers/SearchController.cs
@@ -13,11 +13,13 @@ namespace CryptoHubAPI.Controllers
 
         private readonly IUserFollowerRepository _userFollowerRepository;
         private readonly IUserRepository _userRepository;
+        private readonly ICoinRepository _coinRepository;
 
-        public SearchController(IUserRepository userRepository, IUserFollowerRepository userFollowerRepository)
+        public SearchController(IUserRepository userRepository, IUserFollowerRepository userFollowerRepository, ICoinRepository coinRepository)
         {
             _userFollowerRepository = userFollowerRepository;
             _userRepository = userRepository;
+            _coinRepository = coinRepository;
         }
     }
 }
