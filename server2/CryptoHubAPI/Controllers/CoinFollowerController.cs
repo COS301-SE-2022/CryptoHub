1d40e048939f8e4feb1f4345912d80b24a937796
Thabo  Jerome Chesane <u20507102@tuks.co.za>
2022-07-05 12:55:42 +0200

    Create CoinFollowerController.cs

---
+++ server2/CryptoHubAPI/Controllers/CoinFollowerController.cs
@@ -0,0 +1,21 @@
+﻿using Domain.IRepository;
+using Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CryptoHubAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class CoinFollowerController
+    {
+
+        
+        public CoinFollowerController()
+        {
+        }
+    }
+}
+
