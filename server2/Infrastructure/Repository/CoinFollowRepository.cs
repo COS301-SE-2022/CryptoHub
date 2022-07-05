df5017288bae298c882b2ad5adbf8b3281c1cd06
Thabo  Jerome Chesane <u20507102@tuks.co.za>
2022-07-05 12:59:51 +0200

    Create CoinFollowRepository.cs

---
+++ server2/Infrastructure/Repository/CoinFollowRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.IRepository;
+using Infrastructure.Data;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Infrastructure.Repository
+{
+    public class CoinFollowRepository
+    {
+        public CoinFollowRepository()
+        {
+        }
+    }
+}
+
