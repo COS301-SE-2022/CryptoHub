303cf9d7372fdb3ca49bcc06a91a0c4f4b9c5e32
Khotso-Bore <u19180642@tuks.co.za>
2022-07-06 10:59:49 +0200

    swagger

--- server2/CryptoHubAPI/Program.cs
+++ server2/CryptoHubAPI/Program.cs
@@ -39,11 +39,14 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+/*if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+}*/
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors(
     options =>
