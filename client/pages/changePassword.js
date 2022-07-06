e07b9333b2312f9328e6d8dda778fb3cc3618fd5
Daniel de Nobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-07-06 18:50:02 +0200

    Update changePassword.js

--- client/pages/changePassword.js
+++ client/pages/changePassword.js
@@ -8,7 +8,82 @@ function changePassword() {
     const [error, setError] = useState(false);
     const [loading, setLoading] = useState(false);
   return (
-    <div>changePassword</div>
+    <>
+    <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Change Password
+          </h2>
+        </div>
+        <form className="mt-8 space-y-6" >
+          <input type="hidden" name="remember" defaultValue="true" />
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="email-address" className="sr-only">
+                New Password
+              </label>
+              <input
+                id="newPassword"
+                name="newPassword"
+                type="newPassword"
+                autoComplete="newPassword"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="New Password"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Confirm New Password
+              </label>
+              <input
+                id="confirmPassword"
+                name="confirmPassword"
+                type="confirmPassword"
+                autoComplete="confirmPassword"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Confirm New Password"
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-center">
+            <div className="text-sm">
+              <a
+                href="/forgotPassword"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Back to Forgot Password?
+              </a>
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              {loading ? (
+                <p className="text-indigo-200">Loading...</p>
+              ) : (
+                <p>Confirm</p>
+              )}
+            </button>
+          </div>
+        </form>
+        {error ? (
+          <h2 className="text-center text-sm font-semibold text-red-500">
+            Passwords do not match!
+          </h2>
+        ) : null}
+      </div>
+    </div>
+  </>
   )
 }
 
