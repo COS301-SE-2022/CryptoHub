f46614f5429eecc324e0624cacb93d89734f8b3b
Casparus <u20780479@tuks.co.za>
2022-07-07 20:46:35 +0200

    Added and mapped CommentDTO

--- server2/Infrastructure/AutoMapper/MapperProfile.cs
+++ server2/Infrastructure/AutoMapper/MapperProfile.cs
@@ -3,6 +3,7 @@ using Domain.Models;
 using Infrastructure.DTO.LikeDTOs;
 using Infrastructure.DTO.UserDTOs;
 using Infrastructure.DTO.CoinDTOs;
+using Infrastructure.DTO.CommentDTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,12 +20,14 @@ namespace Infrastructure.AutoMapper
             CreateMap<User, UserDTO>();
             CreateMap<Like, LikeDTO>();
             CreateMap<Coin, CoinDTO>();
+            CreateMap<Comment, CommentDTO>();
             #endregion
 
             #region DTO to Model
             CreateMap<RegisterDTO, User>();
             CreateMap<LikeDTO, Like>();
             CreateMap<CoinDTO, Coin>();
+            CreateMap<CommentDTO, Comment>();
             #endregion
 
         }
---
+++ server2/Infrastructure/DTO/CommentDTOs/CommentDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DTO.CommentDTOs
+{
+    public class CommentDTO
+    {
+        public int CommentId { get; set; }
+        public int UserId { get; set; }
+        public int PostId { get; set; }
+        public string Content { get; set; } = null!;
+    }
+}
