cfd5930c575a092fefecd32f9707374383c219af
Casparus <u20780479@tuks.co.za>
2022-07-07 19:26:29 +0200

    Created CoinService files

---
+++ server2/BusinessLogic/Services/CoinService/CoinService.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Domain.IRepository;
+using Domain.Models;
+using Infrastructure.DTO.CoinDTOs;
+using Infrastructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services.CoinService
+{
+    public class CoinService : ICoinService
+    {
+        private readonly ICoinRepository _coinRepository;
+        private readonly IMapper _mapper;
+        public CoinService(ICoinRepository coinRepository, IMapper mapper)
+        {
+            _coinRepository = coinRepository;
+            _mapper = mapper;
+        }
+        public async Task<List<CoinDTO>> GetAllUsers()
+        {
+            var coins = await _coinRepository.GetAll();
+            return _mapper.Map<List<CoinDTO>>(coins);
+        }
+    }
+}
---
+++ server2/Infrastructure/DTO/CoinDTOs/CoinDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DTO.CoinDTOs
+{
+    public class CoinDTO
+    {
+        public int CoinId { get; set; }
+        public string CoinName { get; set; } = null!;
+
+    }
+}
