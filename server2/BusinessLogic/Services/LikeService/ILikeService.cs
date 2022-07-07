0c91c221843e5fc6ed4004c4620ad2e5604d83f1
Thabo  Jerome Chesane <u20507102@tuks.co.za>
2022-07-07 20:09:05 +0200

    ILikeservice

---
+++ server2/BusinessLogic/Services/LikeService/ILikeService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Domain.Models;
+using Infrastructure.DTO.LikeDTOs;
+
+namespace BusinessLogic.Services.LikeService
+{
+    public interface ILikeService
+    {
+        Task<List<LikeDTO>> GetAllLikes();
+
+        Task<List<LikeDTO>> GetLikeByUserId(int id);
+
+        Task<LikeDTO> GetLikeCountByPostId(int id);
+
+        Task<List<LikeDTO>> GetLikeByCommentId(int id);
+
+        Task<LikeDTO> GetLikeCountByCommentId(int id);
+
+        Task<List<LikeDTO>> GetLikeByReplyId(int id);
+
+        Task<LikeDTO> GetLikeCountByReplyId(int id);
+
+        Task<LikeDTO> GetLikeBy(int userId, int postId);
+
+        Task<LikeDTO> AddLike(Like like);
+
+        Task<LikeDTO> UpdateLike(Like like);
+
+        Task<LikeDTO> Delete(int id);
+
+
+
+    }
+}
+
