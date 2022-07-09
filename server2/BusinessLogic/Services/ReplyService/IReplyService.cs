402542b5dca7f6c0a0ae5e20812ce62a58f9773c
Thabo  Jerome Chesane <u20507102@tuks.co.za>
2022-07-09 20:33:18 +0200

    IReply

---
+++ server2/BusinessLogic/Services/ReplyService/IReplyService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Domain.Models;
+using Infrastructure.DTO.ReplyDTOs;
+
+namespace BusinessLogic.Services.ReplyService
+{
+    public interface IReplyService
+    {
+      Task<List<ReplyDTO>> GetRepliesByUserId(int userId);
+      Task<List<ReplyDTO>> GetRepliesByCommentId(int commentId);
+      Task<ReplyDTO> GetRepliesCountByCommentId(int commentId);
+      Task<ReplyDTO> AddReply(Reply reply);
+      Task<ReplyDTO> UpdateReply(int replyId, Reply reply);
+      Task<ReplyDTO> Delete(int replyId);
+
+
+    }
+}
+
---
+++ server2/BusinessLogic/Services/ReplyService/ReplyService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BusinessLogic.Services.ReplyService
+{
+    public class ReplyService
+    {
+        public ReplyService()
+        {
+        }
+    }
+}
+
