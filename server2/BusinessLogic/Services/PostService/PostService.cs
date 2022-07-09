e861da986bfc5454371d8f0eaa5113cad58b0cdd
Thabo  Jerome Chesane <u20507102@tuks.co.za>
2022-07-09 19:01:41 +0200

    PostService functionality complete

--- server2/BusinessLogic/Services/PostService/PostService.cs
+++ server2/BusinessLogic/Services/PostService/PostService.cs
@@ -36,15 +36,37 @@ namespace BusinessLogic.Services.PostService
 
         public async Task<PostDTO> AddPost(CreatePostDTO createPostDTO)
         {
-          
+            Post post = new Post();
+            if (createPostDTO.ImageDTO != null)
+            {
+                byte[] imageArray = Convert.FromBase64String(createPostDTO.ImageDTO.Blob);
+
+                Image image = new Image();
+                image.Image1 = imageArray;
+
+                await _imageRepository.Add(image);
+                post.ImageId = image.ImageId;
+
+            }
+
+            post.Content = createPostDTO.Post;
+            post.UserId = createPostDTO.UserId;
+
+            return _mapper.Map<PostDTO>(post);
+
         }
         public async Task<PostDTO> UpdatePost(Post Post)
         {
+            var response = await _postRepository.Update(u => u.PostId == Post.PostId, Post);
+            if (response == null)
+                return null;
 
+            return _mapper.Map<PostDTO>(response);
         }
-        public async Task<PostDTO> Delete(int id)
+        public async Task Delete(int id)
         {
-            
+            await _postRepository.DeleteOne(u => u.PostId == id);
+
         }
 
         
