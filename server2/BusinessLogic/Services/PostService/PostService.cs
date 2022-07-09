f1bec92b22ab0dbc364bd4b25283af930a5abc75
Thabo  Jerome Chesane <u20507102@tuks.co.za>
2022-07-09 18:35:16 +0200

    Constructor- PostService

--- server2/BusinessLogic/Services/PostService/PostService.cs
+++ server2/BusinessLogic/Services/PostService/PostService.cs
@@ -1,10 +1,43 @@
 ﻿using System;
+using Domain.IRepository;
+using Domain.Models;
+using Infrastructure.DTO.PostDTO;
+
 namespace BusinessLogic.Services.PostService
 {
-    public class PostService
+    public class PostService : IPostService
     {
-        public PostService()
+        private readonly IPostRepository _postRepository;
+        private readonly IImageRepository _imageRepository;
+        public PostService(IPostRepository postRepository, IImageRepository imageRepository)
         {
+            _postRepository = postRepository;
+            _imageRepository = imageRepository;
+        }
+
+        public Task<PostDTO> AddPost(CreatePostDTO createPostDTO)
+        {
+          
+        }
+
+        public Task<PostDTO> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<PostDTO>> GetAllPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PostDTO> GetPostByUserId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PostDTO> UpdatePost(Post Post)
+        {
+            throw new NotImplementedException();
         }
     }
 }
