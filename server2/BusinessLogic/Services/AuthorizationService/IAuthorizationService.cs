a43d15e44fa2b697544554cceb9ee96ccb54eb36
Khotso-Bore <u19180642@tuks.co.za>
2022-07-09 20:35:58 +0200

    commit

---
+++ server2/BusinessLogic/Services/AuthorizationService/IAuthorizationService.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Models;
+using Infrastructure.DTO.UserDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services.AuthorizationService
+{
+    public interface IAuthorizationService
+    {
+        Task<Response<UserDTO>> Login(LoginDTO loginDTO);
+
+        Task<Response<UserDTO>> Register(RegisterDTO registerDTO);
+    }
+}
---
+++ server2/Infrastructure/AutoMapper/MapperProfile.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Domain.Models;
+using Infrastructure.DTO.RoleDTOs;
+using Infrastructure.DTO.TagDTOs;
+using Infrastructure.DTO.UserDTOs;
+using Infrastructure.DTO.CoinDTOs;
+using Infrastructure.DTO.CommentDTOs;
+using Infrastructure.DTO.LikeDTOs;
+using Infrastructure.DTO.TagDTOs;
+using Infrastructure.DTO.UserCoinDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.DTO.PostDTO;
+using Infrastructure.DTO.UserRoleDTOs;
+using Infrastructure.DTO.ReplyDTOs;
+
+namespace Infrastructure.AutoMapper
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            #region Model to DTO
+            CreateMap<User, UserDTO>();
+            CreateMap<Like, LikeDTO>();
+            CreateMap<Coin, CoinDTO>();
+            CreateMap<Comment, CommentDTO>();
+            CreateMap<Role, RoleDTO>();
+            CreateMap<Tag, TagDTO>();
+            CreateMap<UserRole, UserRoleDTO>();
+            CreateMap<UserCoin, UserCoinDTO>();
+            #endregion
+
+            #region DTO to Model
+            CreateMap<RegisterDTO, User>();
+            CreateMap<LikeDTO, Like>();
+            CreateMap<CoinDTO, Coin>();
+            CreateMap<CommentDTO, Comment>();
+            CreateMap<PostDTO, Post>();
+            CreateMap<UserCoinDTO, UserCoin>();
+            CreateMap<ReplyDTO, Reply>();
+            #endregion
+
+        }
+    }
+}
