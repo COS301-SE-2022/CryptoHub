16a3e5501b96842d0e5faf2a084144208c9a0c1e
Thabo  Jerome Chesane <u20507102@tuks.co.za>
2022-07-18 17:56:01 +0200

    Create PostReport.cs

---
+++ server2/Domain/Models/PostReport.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Domain.Models
+{
+    public class PostReport
+    {
+        public PostReport()
+        {
+        }
+
+
+        public int id { get; set; }
+        public int Userid { get; set; }
+        public int PostId { get; set; }
+
+
+        [JsonIgnore]
+        public virtual User User{ get; set; }
+
+        [JsonIgnore]
+        public virtual Post Post { get; set; }
+
+
+    }
+}
+
