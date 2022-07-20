7de72d427149fbcb7a6562fd7007317328c8da6d
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2022-07-20 20:41:02 +0200

    added swiper for news carousel

--- client/components/InfoSection/News.js
+++ client/components/InfoSection/News.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Article from "./Article";
 import { mockNewsData } from "../../mocks/mockNewsData";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
 
 const News = () => {
   return (
     <div className="flex flew-row justify-start flex-wrap">
       <p className="mt-3 font-bold text-indigo-600">News</p>
-      {mockNewsData.map((data, index) => {
-        return (
-          <Article key={index} title={data.title} content={data.content} />
-        );
-      })}
+
+      <div>
+        <Swiper
+          cssMode={true}
+          navigation={true}
+          pagination={true}
+          mousewheel={true}
+          keyboard={true}
+          modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+          className="mySwiper"
+        >
+          {/* {mockNewsData.map((data, index) => {
+            return (
+              <SwiperSlide>
+                <Article
+                  key={index}
+                  title={data.title}
+                  content={data.content}
+                />
+              </SwiperSlide>
+            );
+          })} */}
+          <SwiperSlide>Hello</SwiperSlide>
+          <SwiperSlide>Hello</SwiperSlide>
+          <SwiperSlide>Hello</SwiperSlide>
+          <SwiperSlide>Hello</SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 };
--- client/mocks/mockNewsData.js
+++ client/mocks/mockNewsData.js
@@ -8,4 +8,22 @@ export const mockNewsData = [
       "Bitcoin price remains in a strong downtrend below the USD 35,000 support level. BTC even dropped below USD 30,000 before recovering. It is currently (04:44 UTC) correcting losses and trading near USD 30,800, but upsides might be limited above USD 32,500. BTC is down almost 9% in a day and 20% in a week.",
     datePublished: "",
   },
+  {
+    id: 1,
+    title:
+      "Bitcoin Tests USD 30K, Ethereum Dives, LUNA Halves as UST Loses Peg",
+    image: "",
+    content:
+      "Bitcoin price remains in a strong downtrend below the USD 35,000 support level. BTC even dropped below USD 30,000 before recovering. It is currently (04:44 UTC) correcting losses and trading near USD 30,800, but upsides might be limited above USD 32,500. BTC is down almost 9% in a day and 20% in a week.",
+    datePublished: "",
+  },
+  {
+    id: 1,
+    title:
+      "Bitcoin Tests USD 30K, Ethereum Dives, LUNA Halves as UST Loses Peg",
+    image: "",
+    content:
+      "Bitcoin price remains in a strong downtrend below the USD 35,000 support level. BTC even dropped below USD 30,000 before recovering. It is currently (04:44 UTC) correcting losses and trading near USD 30,800, but upsides might be limited above USD 32,500. BTC is down almost 9% in a day and 20% in a week.",
+    datePublished: "",
+  },
 ];
