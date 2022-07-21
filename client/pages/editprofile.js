4d498e380c1bdb740877103231e492cbbb782fde
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2022-07-21 19:54:17 +0200

    Update editprofile.js

--- client/pages/editprofile.js
+++ client/pages/editprofile.js
@@ -5,7 +5,7 @@ const editprofile = () => {
   return (
     <Layout>
       <>
-        <div className="mt-5 md:mt-0 md:col-span-2">
+        <div className="mt-5 md:mt-0 md:col-span-2 w-5/12">
           <form action="#" method="POST">
             <div className="shadow sm:rounded-md sm:overflow-hidden">
               <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
