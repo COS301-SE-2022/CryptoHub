70d22e6880597f5c56c3e05ade6a98f3061fa6cf
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2022-07-21 19:43:30 +0200

    added new profile pictures and profile page

--- client/components/Posts/Post.js
+++ client/components/Posts/Post.js
@@ -169,7 +169,15 @@ const Post = ({ name, content, userId, postId, imageId }) => {
   return (
     <div className="bg-white m-4 p-4 rounded-lg">
       <div className="flex flew-row items-center mb-2">
-        <div className="w-8 h-8 bg-black rounded-3xl"></div>
+        <span className="inline-block h-8 w-8 rounded-full overflow-hidden bg-gray-100">
+          <svg
+            className="h-full w-full text-gray-300"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+          </svg>
+        </span>
 
         {user.id == thisUser.userId ? (
           <div class="flex justify-between flex-container">
---
+++ client/pages/editprofile.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const editprofile = () => {
+  return (
+    <Layout>
+      <>
+        <div className="mt-5 md:mt-0 md:col-span-2">
+          <form action="#" method="POST">
+            <div className="shadow sm:rounded-md sm:overflow-hidden">
+              <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+                <div>
+                  <label className="block text-sm font-medium text-gray-700">
+                    Photo
+                  </label>
+                  <div className="mt-1 flex items-center">
+                    <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
+                      <svg
+                        className="h-full w-full text-gray-300"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                      </svg>
+                    </span>
+                    <button
+                      type="button"
+                      className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                      Change
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                <button
+                  type="submit"
+                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Save
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </>
+    </Layout>
+  );
+};
+
+export default editprofile;
