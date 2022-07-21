462d699fafcd87f1576c1eafe23ce9154fbc17ce
Daniel de Nobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-07-21 19:43:24 +0200

    Update Post.js

--- client/components/Posts/Post.js
+++ client/components/Posts/Post.js
@@ -181,15 +181,6 @@ const Post = ({ name, content, userId, postId, imageId }) => {
                   </p>
                 </Link>
               </div>
-
-              {/* <div className="translate-x-50 text-right">
-              <button
-                onClick={() => setShowModal(true)}
-                className="text-sm flex flex-row"
-              >
-                <p className="ml-1"> ... </p>
-              </button>
-            </div> */}
             </div>
           ) : (
             <div class="flex flex-container">
@@ -203,15 +194,6 @@ const Post = ({ name, content, userId, postId, imageId }) => {
                   </p>
                 </Link>
               </div>
-
-              {/* <div>
-              <button
-                onClick={() => setShowModal(true)}
-                className="text-sm flex flex-row"
-              >
-                <p className="ml-1"> ... </p>
-              </button>
-            </div> */}
             </div>
           )}
         </div>
@@ -221,7 +203,63 @@ const Post = ({ name, content, userId, postId, imageId }) => {
               onClick={() => setShowModal(true)}
               className="text-sm flex flex-row"
             >
-              <p className="ml-1"> ... </p>
+              <Menu as="div" className="ml-1 sm:ml-3 relative">
+                <div>
+                  <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                    <span className="sr-only">Open user menu</span>
+                    <div className="w-8 h-8 bg-black rounded-3xl"></div>
+                  </Menu.Button>
+                </div>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <>
+                          <button
+                            onClick={() => {
+                              router.push("/profile");
+                            }}
+                            className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700 w-full"
+                            )}
+                          >
+                            Profile
+                          </button>
+                          <button
+                            onClick={() => {
+                              router.push("/");
+                            }}
+                            className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700 w-full"
+                            )}
+                          >
+                            Feed
+                          </button>
+                          <button
+                            onClick={logout}
+                            className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700 w-full"
+                            )}
+                          >
+                            Sign out
+                          </button>
+                        </>
+                      )}
+                    </Menu.Item>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
             </button>
           </div>
         </div>
