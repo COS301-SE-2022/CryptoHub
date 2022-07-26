b899a4d7ab6427437c0a99afc2ce92fee0b4adb9
Khotso-Bore <u19180642@tuks.co.za>
2022-07-26 21:00:00 +0200

    new chnages

--- client/components/NavigationBar/NavigationProfile.js
+++ client/components/NavigationBar/NavigationProfile.js
@@ -22,6 +22,7 @@ const NavigationProfile = () => {
     fetch(`http://localhost:7215/api/User/GetUserById/${user.id}`, options)
       .then((response) => response.json())
       .then((data) => {
+        console.warn("Image from firebase: ", data.imageUrl);
         setThisUser(data.imageUrl);
       })
       .catch((error) => {});
@@ -61,6 +62,10 @@ const NavigationProfile = () => {
                 <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
               </svg>
             </span>
+          )}
+          {console.warn(
+            "Wwweeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeh ",
+            thisUser
           )}
         </Menu.Button>
       </div>
