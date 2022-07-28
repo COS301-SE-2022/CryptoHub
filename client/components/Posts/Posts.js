be38e5ca972a5aacdee3778168b68b3683587730
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2022-07-28 23:38:21 +0200

    Update Posts.js

--- client/components/Posts/Posts.js
+++ client/components/Posts/Posts.js
@@ -32,14 +32,40 @@ const Posts = () => {
       });
   };
 
+  const handleFeedPosts = () => {
+    setLoading(true);
+
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "bearer " + user.token,
+      },
+    };
+
+    fetch("http://localhost:7215/api/Post/GetFeed", options)
+      .then((response) => response.json())
+      .then((data) => {
+        setLoading(false);
+        let posts = data.reverse();
+        setPosts(posts);
+      })
+      .catch(() => {
+        setError(true);
+        setLoading(false);
+      });
+  };
+
   useEffect(() => {
-    handleGetAllPosts();
+    user.auth ? handleFeedPosts() : handleGetAllPosts();
   }, [feedstate]);
 
   return (
     <div className="sm:w-5/12">
       {loading ? (
         <p>loading...</p>
+      ) : posts.count == 0 ? (
+        <div>Explore</div>
       ) : (
         posts.map((data, index) => {
           return (
