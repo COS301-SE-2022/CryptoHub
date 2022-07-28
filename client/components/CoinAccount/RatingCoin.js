d98db8ddb8445944684ef8b04890404c0711106e
Daniel de Nobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-07-28 22:45:00 +0200

    Update RatingCoin.js

--- client/components/CoinAccount/RatingCoin.js
+++ client/components/CoinAccount/RatingCoin.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function RatingCoin() {
-  return <div>RatingCoin</div>;
+  return (
+    <div class="dropdown">
+      <button class="dropbtn">Dropdown</button>
+      <div class="dropdown-content">
+        <a href="#">1</a>
+        <a href="#">2</a>
+        <a href="#">3</a>
+        <a href="#">4</a>
+        <a href="#">5</a>
+      </div>
+    </div>
+  );
 }
 
 export default RatingCoin;
