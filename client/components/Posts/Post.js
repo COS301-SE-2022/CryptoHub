2e329a7d5fea116da50ee23b6a8c6b3537746c37
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2022-07-28 23:07:19 +0200

    Update Post.js

--- client/components/Posts/Post.js
+++ client/components/Posts/Post.js
@@ -299,7 +299,7 @@ const Post = ({ name, content, userId, postId, imageId, admin, reports }) => {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
                       {!admin && (
                         <Menu.Item>
                           {({ active }) => (
