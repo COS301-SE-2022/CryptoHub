13fc34b884798866f993e6ae43934ae91737d1c6
Daniel de Nobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-07-28 22:59:40 +0200

    Update RatingCoin.js

--- client/components/CoinAccount/RatingCoin.js
+++ client/components/CoinAccount/RatingCoin.js
@@ -52,6 +52,19 @@ export default function Example() {
                     2
                   </a>
                 )}
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={classNames(
+                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                        "block px-4 py-2 text-sm"
+                      )}
+                    >
+                      3
+                    </a>
+                  )}
+                </Menu.Item>
               </Menu.Item>
             </Menu.Item>
           </div>
