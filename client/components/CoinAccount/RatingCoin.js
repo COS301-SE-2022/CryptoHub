cbe5e55ca48fe68999a60f2aff266d83c467f641
Daniel de Nobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-07-28 21:49:15 +0200

    Update RatingCoin.js

--- client/components/CoinAccount/RatingCoin.js
+++ client/components/CoinAccount/RatingCoin.js
@@ -2,3 +2,9 @@ import React, { useState } from "react";
 import Header from "components/Header";
 import Rate from "components/Rate";
 import ExternalInfo from "components/ExternalInfo";
+
+const Rating = () => {
+  const [rating, setRating] = useState(0);
+  const [rating2, setRating2] = useState(0);
+};
+export default Rating;
