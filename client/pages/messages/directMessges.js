699f3386b739d5ae6fa2fd53ef5ab5f19e2597ed
Daniel de Nobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-08-16 20:00:15 +0200

    Update directMessges.js

--- client/pages/messages/directMessges.js
+++ client/pages/messages/directMessges.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import Layout from "../../components/Layout";
-import { useRouter } from "next/router";
-import { useContext } from "react";
-import { userContext } from "../../auth/auth";
-import { getFirestore, serverTimestamp } from "@firebase/firestore";
-import { collection, getDocs, addDoc } from "firebase/firestore";
+import React from "react";
+
+function directMessges() {
+  return <div>directMessges</div>;
+}
+
+export default directMessges;
