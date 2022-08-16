d1f2bcf7a2f4b72e616613c800b95b66a9d43724
Daniel de Nobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-08-16 20:26:25 +0200

    Update directMessges.js

--- client/pages/messages/directMessges.js
+++ client/pages/messages/directMessges.js
@@ -6,33 +6,6 @@ import { userContext } from "../../auth/auth";
 import { getFirestore, serverTimestamp } from "@firebase/firestore";
 import { collection, getDocs, addDoc } from "firebase/firestore";
 
-const handleGetUser = () => {
-  const options = {
-    method: "GET",
-  };
-
-  fetch(`http://localhost:7215/api/User/GetUserById/${id}`, options)
-    .then((response) => response.json())
-    .then((data) => {
-      setUser(data);
-      setUsername(data.username);
-    })
-    .catch((error) => {});
-};
-
-const handleSendMessage = async (e) => {
-  e.preventDefault();
-
-  await addDoc(messagesRef, {
-    sender: user.id.toString(),
-    receiver: id.toString(),
-    message: message,
-    timestamp: serverTimestamp(),
-  });
-  getMessages();
-  setMessage("");
-};
-
 function directMessges() {
   const router = useRouter();
   const { user, app } = useContext(userContext);
@@ -41,6 +14,35 @@ function directMessges() {
   const [username, setUsername] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
+
+  const handleGetUser = () => {
+    const options = {
+      method: "GET",
+    };
+
+    fetch(`http://localhost:7215/api/User/GetUserById/${id}`, options)
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data);
+        setUsername(data.username);
+      })
+      .catch((error) => {});
+  };
+
+  const handleSendMessage = async (e) => {
+    e.preventDefault();
+
+    await addDoc(messagesRef, {
+      sender: user.id.toString(),
+      receiver: id.toString(),
+      message: message,
+      timestamp: serverTimestamp(),
+    });
+    getMessages();
+    setMessage("");
+  };
+
+  useEffect(() => {}, []);
   return <div>directMessges</div>;
 }
 
