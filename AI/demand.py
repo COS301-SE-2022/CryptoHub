bf6111336dec75e027b964f354053a22e67a6b7b
Thabo  Jerome Chesane <u20507102@tuks.co.za>
2022-08-16 21:07:45 +0200

    Update demand.py

--- AI/demand.py
+++ AI/demand.py
@@ -1,8 +1,15 @@
 
 import requests
 
+def get_supply(coinid):
+    x = requests.get('http://api.coincap.io/v2/assets/'+coinid)
+    content = x.json().get('data')
+    return content.get('supply')
+
+
 x =  requests.get('https://api.coincap.io/v2/assets')
 content = x.json().get('data')
 
-for i in range (2):
+
+for i in range (1):
     print(content[i].get('marketCapUsd'))
