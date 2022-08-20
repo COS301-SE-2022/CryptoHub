8858bac369b862f3909b2db677e6170940f5d2a8
Thabo  Jerome Chesane <u20507102@tuks.co.za>
2022-08-20 15:40:00 +0200

    Update demand.py

--- AI/demand.py
+++ AI/demand.py
@@ -1,5 +1,7 @@
 
 import requests
+from csv import DictReader
+
 
 def get_supply(coinid):
     x = requests.get('http://api.coincap.io/v2/assets/'+coinid)
@@ -11,13 +13,25 @@ def get_marketCapUsd(coinid):
     content = x.json().get('data')
     return content.get('marketCapUsd')
 
-def Equation(coinid, x, rating , following):
+def get_rating(coinName):
+    with open('AI/ratings.csv', 'r') as f:
+        reader = DictReader(f)
+        for row in reader:
+            if row['coinName'] == coinName:
+                return row['rating']
+
+
+
+
+def Equation(coinid,coinName, x, rating , following):
     a = get_supply(coinid)
     b = get_marketCapUsd(coinid)
     c = rating
     d = following
     fx = a(x)**3 + b(x)**2 + c(x) + d(x)
     return fx
+
+
 
 x =  requests.get('https://api.coincap.io/v2/assets')
 content = x.json().get('data')
