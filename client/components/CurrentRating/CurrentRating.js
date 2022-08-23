1e495d2e9703a7cc46e785ea692974b505e679b2
Daniel de Nobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-08-23 18:43:00 +0200

    Update CurrentRating.js

--- client/components/CurrentRating/CurrentRating.js
+++ client/components/CurrentRating/CurrentRating.js
@@ -58,7 +58,39 @@ function CurrentRating() {
       .catch(() => {});
   };
 
-  return <div>CurrentRating</div>;
+  useEffect(() => {
+    handleGetCoinRating();
+  }, []);
+
+  return (
+    <Container>
+      {[...Array(5)].map((item, index) => {
+        const givenRating = index + 1;
+
+        return (
+          <label>
+            <Radio
+              type="radio"
+              value={givenRating}
+              onClick={() => {
+                setRate(givenRating);
+                handleRateCoin(givenRating);
+              }}
+            />
+            <Rating>
+              <FaStar
+                color={
+                  givenRating < rate || givenRating === rate
+                    ? "000"
+                    : "rgb(192,192,192)"
+                }
+              />
+            </Rating>
+          </label>
+        );
+      })}
+    </Container>
+  );
 }
 
 export default CurrentRating;
