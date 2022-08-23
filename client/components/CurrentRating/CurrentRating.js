5957be7cd6aab66514e8465e872ac4e7f734862d
Daniel de Nobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-08-23 18:39:15 +0200

    Update CurrentRating.js

--- client/components/CurrentRating/CurrentRating.js
+++ client/components/CurrentRating/CurrentRating.js
@@ -1,4 +1,9 @@
-import React from "react";
+import { FaStar } from "react-icons/fa";
+import { Container, Radio, Rating } from "./RatingStyles";
+import React, { useState, useEffect, useContext } from "react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { userContext } from "../../auth/auth";
 
 function CurrentRating() {
   return <div>CurrentRating</div>;
