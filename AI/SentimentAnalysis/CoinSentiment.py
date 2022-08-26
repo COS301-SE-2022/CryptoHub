8dbbc94092d5e7f601e08df0831e2f591c4fbbe6
Khotso-Bore <u19180642@tuks.co.za>
2022-08-26 17:18:43 +0200

    ai

---
+++ AI/SentimentAnalysis/CoinSentiment.py
@@ -0,0 +1,14 @@
+import imp
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import nltk
+#nltk.download('vader_lexicon')
+from nltk.sentiment import SentimentIntensityAnalyzer
+ 
+sia = SentimentIntensityAnalyzer()
+
+
+x = sia.polarity_scores('The market is so bad, I don’t know how to fee')
+
+print(x)
