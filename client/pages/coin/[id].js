14547e8b4ee7bd46dd1588ad43f4cd73b7a743e6
Daniel de Nobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-08-30 19:43:18 +0200

    Updated coin rating

--- client/pages/coin/[id].js
+++ client/pages/coin/[id].js
@@ -120,6 +120,7 @@ const Coin = () => {
   useEffect(() => {
     checkFollowing();
     handleGetCoin();
+    handleGetCoinRating();
     const interval = setInterval(() => {
       handleGetCoin();
     }, 10000);
@@ -182,6 +183,22 @@ const Coin = () => {
             name="Price"
             price={Math.round(coinData.priceUsd * 100) / 100}
           />
+
+          <div className="bg-white m-4 p-4 rounded-lg w-full">
+            <p className="text-xl font-semibold mb-2 translate-y-1 ml-2 text-left text-gray-700">
+              Average Rating:
+            </p>
+            <div className="flex flex-col mb-2">
+              <p className="ml-2 text-3xl">{AverageRate}</p>
+            </div>
+            <p className="text-xl font-semibold mb-2 translate-y-1 ml-2 text-left text-gray-700">
+              Total Rating:
+            </p>
+            <div className="flex flex-col mb-2">
+              <p className="ml-2 text-3xl">{AverageCount}</p>
+            </div>
+          </div>
+
           <div className="bg-white m-4 p-4 rounded-lg w-full">
             <p className="text-xl font-semibold mb-2 translate-y-1 ml-2 text-left text-gray-700">
               Calculate Price
