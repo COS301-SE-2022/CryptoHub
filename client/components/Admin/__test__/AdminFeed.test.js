dfbff73d1a3a634e569914d033cf44c2e6c65729
Casparus <u20780479@tuks.co.za>
2022-08-31 19:21:46 +0200

    Front end integration template

---
+++ client/components/Admin/__test__/AdminFeed.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import AdminFeed from '../AdminFeed';
+
+
+describe("AdminFeed", () => {
+    it('Should render test the admin feed somehow, please update these description', async () => {
+        // render(<AdminFeed />);
+        // const adminFeed = await screen.findByTestId('adminFeed');
+        // expect(adminFeed).toBeInTheDocument();
+    });
+})
