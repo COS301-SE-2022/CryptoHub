0f5aff0275e8df0d599f1f7c014b965a4391d2e8
Khotso-Bore <u19180642@tuks.co.za>
2022-09-13 22:50:18 +0200

    second async change

--- client/components/InfoSection/News.js
+++ client/components/InfoSection/News.js
@@ -16,16 +16,16 @@ const News = () => {
     const response = await fetch(
       "https://newsapi.org/v2/everything?q=(bitcoin OR doge OR cryptocurrency OR ethereum OR crypto OR litecoin OR tether OR bnb OR binance)&apiKey=e05964f35e4e48e0906ed98905cb2e16&language=en"
     );
-      //.then((response) => response.json())
-      //.then((data) => {
+    //.then((response) => response.json())
+    //.then((data) => {
     const data = await response.json();
     let news = data.articles;
     let finalnews = news.slice(0, 10);
     setNews(finalnews);
-    };
+  };
 
   useEffect(() => {
-    await handleGetNews();
+    handleGetNews();
   }, []);
 
   return (
