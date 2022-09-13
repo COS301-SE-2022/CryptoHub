f7d84f371e78f2a31a2999a21a53568dacde7331
Khotso-Bore <u19180642@tuks.co.za>
2022-09-13 22:49:44 +0200

    commit

--- client/components/InfoSection/News.js
+++ client/components/InfoSection/News.js
@@ -13,16 +13,16 @@ const News = () => {
   const [news, setNews] = useState([]);
 
   const handleGetNews = async () => {
-    fetch(
+    const response = await fetch(
       "https://newsapi.org/v2/everything?q=(bitcoin OR doge OR cryptocurrency OR ethereum OR crypto OR litecoin OR tether OR bnb OR binance)&apiKey=e05964f35e4e48e0906ed98905cb2e16&language=en"
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        let news = data.articles;
-        let finalnews = news.slice(0, 10);
-        setNews(finalnews);
-      });
-  };
+    );
+      //.then((response) => response.json())
+      //.then((data) => {
+    const data = await response.json();
+    let news = data.articles;
+    let finalnews = news.slice(0, 10);
+    setNews(finalnews);
+    };
 
   useEffect(() => {
     await handleGetNews();
