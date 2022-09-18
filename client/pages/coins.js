eda5fac5903d77099744ddfc140b00b799f8bd72
Sino <53974039+sinothandomabhena@users.noreply.github.com>
2022-09-18 18:39:32 +0200

    added auth to all coins page

--- client/pages/coins.js
+++ client/pages/coins.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Head from "next/head";
+import Layout from "../components/Layout";
+import { userContext } from "../auth/auth";
 
 const coins = () => {
+  const { user } = useContext(userContext);
+
+  useEffect(() => {
+    if (!user.auth) {
+      router.push("/");
+    }
+  }, [user]);
+
   return (
     <div>
       <Head>
         <title>All Coins</title>
       </Head>
+      <Layout></Layout>
     </div>
   );
 };
