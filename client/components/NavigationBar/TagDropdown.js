9542bf92102fda6061d0ccaf7740940019513ab4
Daniel de Nobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-09-19 18:53:48 +0200

    Update TagDropdown.js

--- client/components/NavigationBar/TagDropdown.js
+++ client/components/NavigationBar/TagDropdown.js
@@ -25,3 +25,18 @@ export default function Example() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
+              <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Account settings
+                </a>
+              )}
+            </Menu.Item>
