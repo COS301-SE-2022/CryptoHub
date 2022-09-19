fb76e9e140eca27fd7164334343894e0a76241ca
Daniel de Nobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-09-19 18:52:21 +0200

    Update TagDropdown.js

--- client/components/NavigationBar/TagDropdown.js
+++ client/components/NavigationBar/TagDropdown.js
@@ -1,4 +1,10 @@
-import React from "react";
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 function TagDropdown() {
   return <div>TagDropdown</div>;
