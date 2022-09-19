387f42135b96f2fe273f9a1353932fad9d47f461
Daniel de Nobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-09-19 18:53:13 +0200

    Update TagDropdown.js

--- client/components/NavigationBar/TagDropdown.js
+++ client/components/NavigationBar/TagDropdown.js
@@ -6,8 +6,14 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-function TagDropdown() {
-  return <div>TagDropdown</div>;
-}
+export default function Example() {
+    return (
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+            Options
+            <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+          </Menu.Button>
+        </div>
 
-export default TagDropdown;
+
