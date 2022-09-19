9ee830684c4ba67451bb4578b7e2c4c54600d6b7
Daniel de Nobrega <92922089+DanieldeNobrega@users.noreply.github.com>
2022-09-19 18:54:18 +0200

    Update TagDropdown.js

--- client/components/NavigationBar/TagDropdown.js
+++ client/components/NavigationBar/TagDropdown.js
@@ -53,3 +53,16 @@ export default function Example() {
                 </a>
               )}
             </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  License
+                </a>
+              )}
+            </Menu.Item>
