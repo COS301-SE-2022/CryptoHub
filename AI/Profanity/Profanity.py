621a641c1bea354db644987cc513b886d87ba68f
Thabo  Jerome Chesane <u20507102@tuks.co.za>
2022-09-19 21:08:29 +0200

    Update Profanity.py

--- AI/Profanity/Profanity.py
+++ AI/Profanity/Profanity.py
@@ -3,6 +3,19 @@ from better_profanity import profanity
 text = ''
 print(profanity.contains_profanity(text))
 
+def CheckForProfanity(text):
+    return profanity.contains_profanity(text)
+
+def sendToAdmin(text):
+    x = CheckForProfanity(text)
+    if x == True:
+        print("Profanity detected")
+        #Khotso send to Admin
+    elif x == False:
+        print("No profanity detected")
+        #Khotso dont send to Admin
+
+
 
 def main():
     print("go")
