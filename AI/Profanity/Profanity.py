123f38a3f224df95b8d28b4f351ab42d5a4101e8
Thabo  Jerome Chesane <u20507102@tuks.co.za>
2022-09-19 20:51:52 +0200

    Update Profanity.py

--- AI/Profanity/Profanity.py
+++ AI/Profanity/Profanity.py
@@ -2,3 +2,13 @@ from better_profanity import profanity
 
 text = ''
 print(profanity.contains_profanity(text))
+
+
+def main():
+    print("go")
+    print(profanity.contains_profanity(text))
+    print("done")
+
+
+if __name__ == "__main__":
+    main()
