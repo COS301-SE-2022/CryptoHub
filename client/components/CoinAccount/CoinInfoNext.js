56754e1d8af9b4c314d7c30764feb8f5bbcbd795
Sino <53974039+sinothandomabhena@users.noreply.github.com>
2022-09-20 20:52:52 +0200

    Update CoinInfoNext.js

--- client/components/CoinAccount/CoinInfoNext.js
+++ client/components/CoinAccount/CoinInfoNext.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
 
 const CoinInfoNext = ({ id, name, state, arrow }) => {
   const [fetchedData, setFetchedData] = useState([]);
